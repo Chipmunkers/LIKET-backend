ad74f589a804502354d0cd537c976f2cb04374ad
jochongs <joch2712@naver.com>
2023-10-02 20:58:57 +0900

    feat: payload 타입 고정

--- src/types/TokenPayload.d.ts
+++ src/types/TokenPayload.d.ts
@@ -1,3 +1,13 @@
-export interface TokenPayload {
-    userIdx: number
-} 
+import * as jwt from 'jsonwebtoken';
+
+export type TokenPayload = {
+    email: string,
+    provider: 'local' | 'naver' | 'kakao' | 'apple'
+}
+
+declare module 'jsonwebtoken' {
+    export interface JwtPayload {
+        userIdx: number,
+        provider: 'local' | 'naver' | 'kakao' | 'apple'
+    }
+}
