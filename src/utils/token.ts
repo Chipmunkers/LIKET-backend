e14262f01383dfa08bde0277564f5ee85159386b
jochongs <joch2712@naver.com>
2023-10-02 20:58:16 +0900

    feat: 토큰 검증 예외처리 추가

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import env from '../config/env';
 import { TokenPayload } from '../types/TokenPayload';
 
-export const createToken = (payload: TokenPayload | { email: string, provider: 'local' | 'naver' | 'kakao' | 'apple' }, expiresIn = '24h'): string => {
+export const createToken = (payload: TokenPayload, expiresIn = '24h'): string => {
     const issuer = '';
 
     return jwt.sign(
@@ -15,9 +15,14 @@ export const createToken = (payload: TokenPayload | { email: string, provider: '
     )
 }
 
-export const verifyToken = (token: string) => {
+export const verifyToken = (token: string): jwt.JwtPayload | null => {
     try {
-        return jwt.verify(token, env.jwtSecretKey as string);
+        const verifiedData = jwt.verify(token, env.jwtSecretKey as string);
+
+        if (typeof verifiedData === 'string')
+            return null;
+
+        return verifiedData;
     } catch (err) {
         return null;
     }
