fd74f386bc2f39a129bc2abcbb8659bc056ca7c3
jochongs <joch2712@naver.com>
2024-02-12 18:42:05 +0900

    refactor: change prisma directory path to common

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaService } from '../../common/prisma/prisma.service';
+import { SendEmailVerificationCodeDto } from './dto/request/SendEmailVerificationCodeDto';
 
 @Injectable()
 export class AuthService {
---
+++ src/api/auth/dto/request/SendEmailVerificationCodeDto.ts
@@ -0,0 +1,3 @@
+export class SendEmailVerificationCodeDto {
+  email: string;
+}
