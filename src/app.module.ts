d1a098bb7c559f68a94274fef4f25c630f23b43f
jochongs <joch2712@naver.com>
2024-02-12 18:06:55 +0900

    feat: add modules of all domain

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { BannerModule } from './banner/banner.module';
+import { CultureContentModule } from './culture-content/culture-content.module';
+import { InquiryModule } from './inquiry/inquiry.module';
+import { LiketModule } from './liket/liket.module';
+import { ReveiwModule } from './reveiw/reveiw.module';
+import { RouteModule } from './route/route.module';
+import { TosModule } from './tos/tos.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule, BannerModule, CultureContentModule, InquiryModule, LiketModule, ReveiwModule, RouteModule, TosModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
