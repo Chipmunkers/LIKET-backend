0804c159392d096db000dc97f89b39a6d682e89f
jochongs <joch2712@naver.com>
2024-02-13 19:40:44 +0900

    feat: add inquiry service and controller

--- src/api/inquiry/inquiry.module.ts
+++ src/api/inquiry/inquiry.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../common/prisma/prisma.module';
+import { InquiryService } from './inquiry.service';
+import { InquiryController } from './inquiry.controller';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [InquiryService],
+  controllers: [InquiryController],
+})
 export class InquiryModule {}
