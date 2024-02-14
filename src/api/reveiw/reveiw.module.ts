b27f8e62c176aebe51ed29f11752d96ef5967486
jochongs <joch2712@naver.com>
2024-02-14 15:19:22 +0900

    feat: add review service and controller

--- src/api/reveiw/reveiw.module.ts
+++ src/api/reveiw/reveiw.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../common/prisma/prisma.module';
+import { ReviewController } from './review.controller';
+import { ReviewService } from './review.service';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  controllers: [ReviewController],
+  providers: [ReviewService],
+})
 export class ReveiwModule {}
