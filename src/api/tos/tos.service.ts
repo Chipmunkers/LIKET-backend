f89e71a2e54731827c5af0d8c056389524d5d6a8
jochongs <joch2712@naver.com>
2024-02-15 15:22:16 +0900

    refactor: modify admin service method name

--- src/api/tos/tos.service.ts
+++ src/api/tos/tos.service.ts
@@ -8,21 +8,42 @@ import { TosEntity } from './entity/TosEntity';
 export class TosService {
   constructor(private readonly prisma: PrismaService) {}
 
+  /**
+   * 약관 모두 보기
+   */
   public getTosAll: () => Promise<TosEntity<'summary', 'user'>>;
 
+  /**
+   * 약관 자세히보기
+   */
   public getTosByIdx: () => Promise<TosEntity<'detail', 'user'>>;
 
   // Admin
 
-  public getTosAdminAll: () => Promise<TosEntity<'summary', 'admin'>[]>;
+  /**
+   * 관리자용 약관 모두보기
+   */
+  public getTosAllForAdmin: () => Promise<TosEntity<'summary', 'admin'>[]>;
 
-  public getTosAmdminByIdx: (
+  /**
+   * 관리자용 약관 자세히보기
+   */
+  public getTosByIdxForAdmin: (
     idx: number,
   ) => Promise<TosEntity<'detail', 'admin'>>;
 
+  /**
+   * 약관 생성하기
+   */
   public createTos: (createDto: CreateTosDto) => Promise<void>;
 
+  /**
+   * 약관 수정하기
+   */
   public updateTos: (idx: number, updateDto: UpdateTosDto) => Promise<void>;
 
+  /**
+   * 약관 삭제하기
+   */
   public deleteTos: (idx: number) => Promise<void>;
 }
