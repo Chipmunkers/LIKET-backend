631634dccfd7ba45dc4e376e15593b65a49cd2dd
jochongs <joch2712@naver.com>
2024-02-17 20:51:52 +0900

    test: add signup test code

--- src/api/user/user.service.spec.ts
+++ src/api/user/user.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from '../../common/prisma/prisma.service';
+import { AuthService } from '../auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('UserService', () => {
   let service: UserService;
+  let prismaMock: PrismaService;
+  let authServiceMock: AuthService;
+  let jwtServiceMock: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {},
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    prismaMock = module.get<PrismaService>(PrismaService);
+    authServiceMock = module.get<AuthService>(AuthService);
+    jwtServiceMock = module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Sign Up success case', async () => {
+    authServiceMock.verifyEmailAuthToken = jest.fn().mockReturnValue(true);
+    prismaMock.user.findFirst = jest.fn().mockResolvedValue(null);
+    jwtServiceMock.verify = jest.fn().mockReturnValue('this.is.token');
+
+    const result = await service.signUp({
+      emailToken: 'jwt token',
+      pw: 'abc123',
+      nickname: 'test',
+    });
+
+    expect(result).toBeInstanceOf(InvalidEmailAuthToken);
   });
 });
