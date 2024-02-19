d81704a60c4f979376c51f20b57116e9d63f057e
jochongs <joch2712@naver.com>
2024-02-19 16:10:43 +0900

    feat: modify JwtService config in AuthModule

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -3,12 +3,23 @@ import { PrismaModule } from '../../common/prisma/prisma.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { HashService } from '../../common/service/hash.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('jwt'),
+        global: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, HashService, JwtService],
+  providers: [AuthService, HashService],
   exports: [AuthService],
 })
 export class AuthModule {}
