3393b999b6f9e59bb1b97e0b090390750d593d34
jochongs <joch2712@naver.com>
2024-02-19 15:24:11 +0900

    feat: add JwtService DI

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { PrismaModule } from '../../common/prisma/prisma.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { HashService } from '../../common/service/hash.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService, HashService],
+  providers: [AuthService, HashService, JwtService],
   exports: [AuthService],
 })
 export class AuthModule {}
