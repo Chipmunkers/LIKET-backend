c93e5ef9b27176f4700c17a3b989366ca47fad1f
jochongs <joch2712@naver.com>
2024-02-19 16:22:09 +0900

    style: add code comment in auth service

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -14,17 +14,32 @@ export class AuthService {
     private readonly redis: RedisService,
   ) {}
 
+  /**
+   * 로그인하기
+   */
   public login: (loginDto: LoginDto) => Promise<string>;
 
+  /**
+   * 이메일 인증번호 발송하기
+   */
   public sendEmailVerificationCode: (
     sendDto: SendEmailVerificationCodeDto,
   ) => Promise<void>;
 
+  /**
+   * 이메일 인증번호 확인하기
+   */
   public checkEmailVerificatioCode: (
     checkDto: CheckEmailVerificationCodeDto,
   ) => Promise<boolean>;
 
+  /**
+   * 이메일 인증 토큰 검사하기
+   */
   public verifyEmailAuthToken: (emailToken: string) => boolean;
 
+  /**
+   * 이메일 인증 토큰 생성하기
+   */
   public signEmailAuthToken: (email: string) => string;
 }
