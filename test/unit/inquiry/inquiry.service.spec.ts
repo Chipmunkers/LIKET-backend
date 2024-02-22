2c97e30fc062539c634fcd66bde453b848bf7ee1
jochongs <joch2712@naver.com>
2024-02-22 19:46:31 +0900

    test: add getInquiryByIdx test cases

---
+++ test/unit/inquiry/inquiry.service.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { InquiryService } from '../../../src/api/inquiry/inquiry.service';
+import { PrismaService } from '../../../src/common/prisma/prisma.service';
+import { InquiryEntity } from '../../../src/api/inquiry/entity/InquiryEntity';
+
+describe('InquiryService', async () => {
+  let service: InquiryService;
+  let prismaMock: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        InquiryService,
+        {
+          provide: PrismaService,
+          useValue: {
+            inquiry: {},
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<InquiryService>(InquiryService);
+  });
+
+  it('getInquiryByIdx success', async () => {
+    // 1. get inquiry with prisma
+    prismaMock.inquiry.findUnique = jest.fn().mockResolvedValue({
+      idx: 1,
+    });
+
+    await expect(service.getInquiryByIdx(1)).resolves.toBeInstanceOf(
+      InquiryEntity<'detail'>,
+    );
+    expect(prismaMock.inquiry.findUnique).toHaveBeenCalledTimes(1);
+  });
+
+  it('getInquiryByIdx fail - not found inquiry', async () => {
+    // not found
+    prismaMock.inquiry.findUnique = jest.fn().mockResolvedValue(null);
+
+    await expect(service.getInquiryByIdx(1)).rejects.toThrow();
+  });
+});
