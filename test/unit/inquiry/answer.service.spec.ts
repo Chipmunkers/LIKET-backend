b44dbbb111b5d8ea1f204fb7c59d048e0995fef9
jochongs <joch2712@naver.com>
2024-02-23 16:51:16 +0900

    test: add createAnswer test cases

--- test/unit/inquiry/answer.service.spec.ts
+++ test/unit/inquiry/answer.service.spec.ts
@@ -3,6 +3,8 @@ import { AnswerService } from '../../../src/api/inquiry/answer.service';
 import { PrismaService } from '../../../src/common/prisma/prisma.service';
 import { AnswerEntity } from '../../../src/api/inquiry/entity/AnswerEntity';
 import { AnswerNotFoundException } from '../../../src/api/inquiry/exception/AnswerNotFoundException';
+import { CreateAnswerDto } from '../../../src/api/inquiry/dto/CreateAnswerDto';
+import { ConflictException } from '@nestjs/common';
 
 describe('AnswerService', () => {
   let service: AnswerService;
@@ -42,5 +44,32 @@ describe('AnswerService', () => {
     await expect(service.getAnswerByIdx(1)).rejects.toThrow(
       AnswerNotFoundException,
     );
+  });
+
+  it('createAnswer success', async () => {
+    // 1. get answer with inquiry idx
+    prismaMock.answer.findFirst = jest.fn().mockResolvedValue(null);
+
+    // 2. create answer
+    prismaMock.answer.create = jest.fn().mockResolvedValue({
+      idx: 1,
+    });
+
+    await expect(service.createAnswer(1, {} as CreateAnswerDto)).resolves.toBe(
+      1,
+    );
+    expect(prismaMock.answer.findFirst).toHaveBeenCalledTimes(1);
+    expect(prismaMock.answer.create).toHaveBeenCalledTimes(1);
+  });
+
+  it('createAnswer fail - inquiry already has answer', async () => {
+    // inquiry already has answer
+    prismaMock.answer.findFirst = jest.fn().mockResolvedValue({
+      idx: 1,
+    });
+
+    await expect(
+      service.createAnswer(1, {} as CreateAnswerDto),
+    ).rejects.toThrow(ConflictException);
   });
 });
