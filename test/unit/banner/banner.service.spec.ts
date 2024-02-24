96b1c2fa5b9413aa9f313661c06765e9e2a8b362
jochongs <joch2712@naver.com>
2024-02-24 12:06:32 +0900

    test: add test cases about banner

--- test/unit/banner/banner.service.spec.ts
+++ test/unit/banner/banner.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BannerService } from '../../../src/api/banner/banner.service';
 import { PrismaService } from '../../../src/common/prisma/prisma.service';
 import { BannerEntity } from '../../../src/api/banner/entity/BannerEntity';
+import { BannerNotFoundException } from '../../../src/api/banner/exception/BannerNotFoundException';
 
 describe('BannerService', () => {
   let service: BannerService;
@@ -23,5 +24,50 @@ describe('BannerService', () => {
 
     service = module.get<BannerService>(BannerService);
     prismaMock = module.get<PrismaService>(PrismaService);
+  });
+
+  it('getBannerAll success', async () => {
+    // 1. get active banner all with prisma
+    prismaMock.activeBanner.findMany = jest.fn().mockResolvedValue([]);
+
+    await expect(service.getBannerAll()).resolves.toBeInstanceOf(
+      BannerEntity<'active'>,
+    );
+    expect(prismaMock.activeBanner.findMany).toHaveBeenCalledTimes(1);
+  });
+
+  it('getBannerAllForAdmin success', async () => {
+    // 1. get banner all with prisma
+    prismaMock.banner.findMany = jest.fn().mockResolvedValue([]);
+
+    const result = await service.getBannerAllForAdmin({
+      page: 1,
+      order: 'desc',
+    });
+
+    for (const banner of result.bannerList) {
+      expect(banner).toBeInstanceOf(BannerEntity);
+    }
+    expect(prismaMock.banner.findMany).toHaveBeenCalledTimes(1);
+  });
+
+  it('getBannerByIdxForAdmin success', async () => {
+    prismaMock.banner.findUnique = jest.fn().mockResolvedValue({
+      idx: 1,
+    });
+
+    await expect(service.getBannerByIdxForAdmin(1)).resolves.toBeInstanceOf(
+      BannerEntity<'all'>,
+    );
+    expect(prismaMock.banner.findUnique).toHaveBeenCalledTimes(1);
+  });
+
+  it('getBannerByIdxForAdmin fail - not found', async () => {
+    prismaMock.banner.findUnique = jest.fn().mockResolvedValue(null);
+
+    await expect(service.getBannerByIdxForAdmin(1)).rejects.toThrow(
+      BannerNotFoundException,
+    );
+    expect(prismaMock.banner.findUnique).toHaveBeenCalledTimes(1);
   });
 });
