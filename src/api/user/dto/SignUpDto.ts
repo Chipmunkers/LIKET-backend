80e9342313344bdc5db3b595030d379027881f11
jochongs <joch2712@naver.com>
2024-02-25 16:34:35 +0900

    feat: add decorator in signUp DTO

--- src/api/user/dto/SignUpDto.ts
+++ src/api/user/dto/SignUpDto.ts
@@ -1,10 +1,44 @@
+import {
+  IsIn,
+  IsInt,
+  IsJWT,
+  IsString,
+  IsStrongPassword,
+  Length,
+  Matches,
+  Max,
+  Min,
+} from 'class-validator';
 import { UploadFileDto } from '../../upload/dto/UploadFileDto';
+import { Optional } from '@nestjs/common';
 
 export class SignUpDto {
+  @IsJWT()
   emailToken: string;
+
+  @IsStrongPassword({
+    minNumbers: 1,
+    minSymbols: 1,
+    minLowercase: 1,
+  })
+  @Length(6, 20)
   pw: string;
+
+  @Matches('^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,8}$')
   nickname: string;
+
+  @IsInt()
+  @IsIn([1, 2])
+  @Optional()
   gender?: 1 | 2;
+
+  @IsInt()
+  @Min(1900)
+  @Max(new Date().getFullYear())
+  @Optional()
   birth?: number;
+
+  @Optional()
+  @IsString()
   profileImg?: UploadFileDto;
 }
