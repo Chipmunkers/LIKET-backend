fe0d1688184482065a44bcad8262d13c754af1e0
jochongs <joch2712@naver.com>
2024-02-25 16:35:44 +0900

    feat: add signUp service method

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -7,15 +7,68 @@ import { UserEntity } from './entity/UserEntity';
 import { MyInfoEntity } from './entity/MyInfoEntity';
 import { UpdateProfileDto } from './dto/UpdateProfileDto';
 import { AuthService } from '../auth/auth.service';
+import { DuplicateUserException } from './exception/DuplicateUserException';
+import { HashService } from '../../common/service/hash.service';
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly authService: AuthService,
+    private readonly hashService: HashService,
   ) {}
 
-  public signUp: (signUpDto: SignUpDto) => Promise<string>;
+  public signUp: (signUpDto: SignUpDto) => Promise<string> = async (
+    signUpDto,
+  ) => {
+    const email = this.authService.verifyEmailAuthToken(signUpDto.emailToken);
+
+    const duplicatedUser = await this.prisma.user.findFirst({
+      where: {
+        OR: [
+          {
+            email,
+          },
+          {
+            nickname: signUpDto.nickname,
+          },
+        ],
+        deletedAt: null,
+      },
+    });
+
+    if (duplicatedUser?.email === email) {
+      throw new DuplicateUserException<'email'>(
+        'This email is already in use',
+        'email',
+      );
+    }
+
+    if (duplicatedUser?.nickname === signUpDto.nickname) {
+      throw new DuplicateUserException<'nickname'>(
+        'This nickname is already in use',
+        'nickname',
+      );
+    }
+
+    const signUpUser = await this.prisma.user.create({
+      data: {
+        email,
+        pw: this.hashService.hashPw(signUpDto.pw),
+        nickname: signUpDto.nickname,
+        birth: signUpDto.birth,
+        profileImgPath: signUpDto.profileImg?.fileName,
+        gender: signUpDto.gender,
+      },
+    });
+
+    const loginAccessToken = this.authService.signLoginAccessToken(
+      signUpUser.idx,
+      signUpUser.isAdmin,
+    );
+
+    return loginAccessToken;
+  };
 
   public getMyInfo: (userIdx: number) => Promise<MyInfoEntity>;
 
