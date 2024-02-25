54f06d0d0a1407cdfe34641891dd6f47d7266b1c
jochongs <joch2712@naver.com>
2024-02-25 14:16:47 +0900

    feat: add POST /auth/local API

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,13 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/LoginDto';
+import { LoginResponseDto } from './dto/response/LoginResponseDto';
+import { TypedException } from '@nestia/core';
+import { ExceptionDto } from '../../common/dto/ExceptionDto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // sample
-  public async login(loginDto: LoginDto): Promise<{ token: string }> {
+  /**
+   * Login API.
+   * @summary Login API.
+   *
+   * @tag Auth
+   */
+  @Post('/local')
+  @HttpCode(200)
+  @TypedException<ExceptionDto>(400, 'Invalid email or password format')
+  @TypedException<ExceptionDto>(401, 'Wrong email or password')
+  @TypedException<ExceptionDto>(500, 'Server Error')
+  public async login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
     const token = await this.authService.login(loginDto);
 
     return { token };
