fdeb9012f63a04c220f81330b816239496b1585f
jochongs <joch2712@naver.com>
2024-02-27 18:35:24 +0900

    feat: modify ReviewListPagerbleDto

--- src/api/review/dto/ReviewListPagerbleDto.ts
+++ src/api/review/dto/ReviewListPagerbleDto.ts
@@ -1,6 +1,18 @@
-export class ReviewListPagerbleDto {
-  page: number;
-  searchby: 'idx' | 'nickname' | 'contents';
-  search: string;
-  order: 'desc' | 'asc';
+import { PickType } from '@nestjs/swagger';
+import { PagenationDto } from '../../../common/dto/PagenationDto';
+import { IsIn, IsOptional, IsString, Length } from 'class-validator';
+
+export class ReviewListPagerbleDto extends PickType(PagenationDto, ['page']) {
+  @IsString()
+  @IsIn(['idx', 'nickname', 'contents'])
+  searchby?: 'idx' | 'nickname' | 'contents';
+
+  @IsString()
+  @Length(1, 20)
+  @IsOptional()
+  search?: string;
+
+  @IsString()
+  @IsIn(['desc', 'asc'])
+  order: 'desc' | 'asc' = 'desc';
 }
