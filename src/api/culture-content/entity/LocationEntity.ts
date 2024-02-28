e73ad223cb104a82ba3896d2d432b5ab315a74b3
jochongs <joch2712@naver.com>
2024-02-28 15:08:17 +0900

    refacotr: modify LocationEntity

--- src/api/culture-content/entity/LocationEntity.ts
+++ src/api/culture-content/entity/LocationEntity.ts
@@ -1,16 +1,34 @@
 import { Location } from '@prisma/client';
 
 export class LocationEntity<T extends 'summary' | 'detail' = 'detail'> {
+  public region1Depth: string;
+  public region2Depth: string;
+  public detailAddress: T extends 'detail' ? string : undefined;
+  public address: T extends 'detail' ? string : undefined;
+  public positionX: T extends 'detail' ? number : undefined;
+  public positionY: T extends 'detail' ? number : undefined;
+  public hCode: T extends 'detail' ? string : undefined;
+  public bCode: T extends 'detail' ? string : undefined;
+
   constructor(
-    public region1Depth: string,
-    public region2Depth: string,
-    public detailAddress: T extends 'detail' ? string : undefined,
-    public address: T extends 'detail' ? string : undefined,
-    public positionX: T extends 'detail' ? number : undefined,
-    public positionY: T extends 'detail' ? number : undefined,
-    public hCode: T extends 'detail' ? string : undefined,
-    public bCode: T extends 'detail' ? string : undefined,
-  ) {}
+    region1Depth: string,
+    region2Depth: string,
+    detailAddress: T extends 'detail' ? string : undefined,
+    address: T extends 'detail' ? string : undefined,
+    positionX: T extends 'detail' ? number : undefined,
+    positionY: T extends 'detail' ? number : undefined,
+    hCode: T extends 'detail' ? string : undefined,
+    bCode: T extends 'detail' ? string : undefined,
+  ) {
+    this.region1Depth = region1Depth;
+    this.region2Depth = region2Depth;
+    this.detailAddress = detailAddress;
+    this.address = address;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.hCode = hCode;
+    this.bCode = bCode;
+  }
 
   static createDetailLocation(data: Location): LocationEntity<'detail'> {
     return new LocationEntity(
