0e1a02ea5fc06ab8495ea673ac368bbf4a2e1511
jochongs <joch2712@naver.com>
2024-02-28 14:27:11 +0900

    feat: change auth module to global module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaModule } from '../../common/prisma/prisma.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { HashService } from '../../common/service/hash.service';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RedisModule } from '../../common/redis/redis.module';
 
+@Global()
 @Module({
   imports: [
     PrismaModule,
