7ea5d84e1cd647308c7a3f2ccb2894c5ad6746a9
jochongs <joch2712@naver.com>
2024-03-01 18:24:02 +0900

    feat: add inquiry type methods

--- src/api/inquiry/inquiry-type.service.ts
+++ src/api/inquiry/inquiry-type.service.ts
@@ -1,12 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../common/prisma/prisma.service';
 import { InquiryTypeEntity } from './entity/InquiryTypeEntity';
+import { InquiryTypeNotFoundException } from './exception/InquiryTypeNotFoundException';
 
 @Injectable()
 export class InquiryTypeService {
   constructor(private readonly prisma: PrismaService) {}
 
-  public getTypeAll: () => Promise<InquiryTypeEntity>;
+  public getTypeAll: () => Promise<InquiryTypeEntity[]> = async () => {
+    const typeList = await this.prisma.inquiryType.findMany({
+      select: {
+        idx: true,
+        name: true,
+        createdAt: true,
+      },
+    });
 
-  public getTypeByIdx: (idx: number) => Promise<InquiryTypeEntity>;
+    return typeList.map((type) => InquiryTypeEntity.createInquiryType(type));
+  };
+
+  public getTypeByIdx: (idx: number) => Promise<InquiryTypeEntity> = async (
+    idx,
+  ) => {
+    const type = await this.prisma.inquiryType.findUnique({
+      select: {
+        idx: true,
+        name: true,
+        createdAt: true,
+      },
+      where: {
+        idx,
+      },
+    });
+
+    if (!type) {
+      throw new InquiryTypeNotFoundException('Cannot find inquiry type');
+    }
+
+    return InquiryTypeEntity.createInquiryType(type);
+  };
 }
