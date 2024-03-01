d32079f4b8cda572c49becf172e2feacc5cb868e
jochongs <joch2712@naver.com>
2024-03-01 18:32:09 +0900

    test: troubleshoot inquiry bug

--- test/unit/inquiry/inquiry.service.spec.ts
+++ test/unit/inquiry/inquiry.service.spec.ts
@@ -4,7 +4,7 @@ import { PrismaService } from '../../../src/common/prisma/prisma.service';
 import { InquiryEntity } from '../../../src/api/inquiry/entity/InquiryEntity';
 import { InquiryNotFoundException } from '../../../src/api/inquiry/exception/InquiryNotFoundException';
 
-describe('InquiryService', async () => {
+describe('InquiryService', () => {
   let service: InquiryService;
   let prismaMock: PrismaService;
 
@@ -22,12 +22,18 @@ describe('InquiryService', async () => {
     }).compile();
 
     service = module.get<InquiryService>(InquiryService);
+    prismaMock = module.get<PrismaService>(PrismaService);
   });
 
   it('getInquiryByIdx success', async () => {
     // 1. get inquiry with prisma
     prismaMock.inquiry.findUnique = jest.fn().mockResolvedValue({
       idx: 1,
+      Answer: [],
+      InquiryType: {
+        idx: 1,
+      },
+      InquiryImg: [],
     });
 
     await expect(service.getInquiryByIdx(1)).resolves.toBeInstanceOf(
