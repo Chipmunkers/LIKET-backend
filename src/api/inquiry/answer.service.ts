e5b2d8cad81358e791eeea64e20c8e8bc68893eb
jochongs <joch2712@naver.com>
2024-03-02 14:57:09 +0900

    feat: add answer service methods

--- src/api/inquiry/answer.service.ts
+++ src/api/inquiry/answer.service.ts
@@ -1,19 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../common/prisma/prisma.service';
 import { CreateAnswerDto } from './dto/CreateAnswerDto';
 import { UpdateAnswerDto } from './dto/UpdateAnswerDto';
 import { AnswerEntity } from './entity/AnswerEntity';
+import { AnswerNotFoundException } from './exception/AnswerNotFoundException';
+import { InquiryNotFoundException } from './exception/InquiryNotFoundException';
 
 @Injectable()
 export class AnswerService {
   constructor(private readonly prisma: PrismaService) {}
 
-  public getAnswerByIdx: (idx: number) => Promise<AnswerEntity>;
+  public getAnswerByIdx: (idx: number) => Promise<AnswerEntity> = async (
+    idx,
+  ) => {
+    const answer = await this.prisma.answer.findUnique({
+      where: {
+        idx,
+        deletedAt: null,
+        Inquiry: {
+          deletedAt: null,
+          User: {
+            deletedAt: null,
+          },
+        },
+      },
+    });
+
+    if (!answer) {
+      throw new AnswerNotFoundException('Cannot find answer');
+    }
+
+    return AnswerEntity.createAnswer(answer);
+  };
 
   public createAnswer: (
     inquiryIdx: number,
     createDto: CreateAnswerDto,
-  ) => Promise<number>;
+  ) => Promise<number> = async (inquiryIdx, createDto) => {
+    const createdAnswer = await this.prisma.$transaction(
+      async (tx) => {
+        const inquiry = await tx.inquiry.findUnique({
+          select: {
+            Answer: {
+              where: {
+                deletedAt: null,
+              },
+            },
+          },
+          where: {
+            idx: inquiryIdx,
+            deletedAt: null,
+            User: {
+              deletedAt: null,
+            },
+          },
+        });
+
+        if (!inquiry) {
+          throw new InquiryNotFoundException('Cannot find inquiry');
+        }
+
+        if (inquiry.Answer[0]) {
+          throw new ConflictException('Already answered inquiry');
+        }
+
+        const createdAnswer = await tx.answer.create({
+          data: {
+            inquiryIdx,
+            contents: createDto.contents,
+          },
+        });
+
+        return createdAnswer;
+      },
+      {
+        isolationLevel: 'RepeatableRead',
+      },
+    );
+
+    return createdAnswer.idx;
+  };
 
   public updateAnswer: (
     idx: number,
--- src/api/inquiry/dto/CreateAnswerDto.ts
+++ src/api/inquiry/dto/CreateAnswerDto.ts
@@ -1,3 +1,7 @@
+import { IsString, Length } from 'class-validator';
+
 export class CreateAnswerDto {
+  @IsString()
+  @Length(1, 200)
   contents: string;
 }
