086e601510e161efc4aebf613b4fd14b8bcccf66
jochongs <joch2712@naver.com>
2024-03-02 14:56:47 +0900

    test: modify answer service method test cases

--- test/unit/inquiry/answer.service.spec.ts
+++ test/unit/inquiry/answer.service.spec.ts
@@ -5,6 +5,7 @@ import { AnswerEntity } from '../../../src/api/inquiry/entity/AnswerEntity';
 import { AnswerNotFoundException } from '../../../src/api/inquiry/exception/AnswerNotFoundException';
 import { CreateAnswerDto } from '../../../src/api/inquiry/dto/CreateAnswerDto';
 import { ConflictException } from '@nestjs/common';
+import { InquiryNotFoundException } from '../../../src/api/inquiry/exception/InquiryNotFoundException';
 
 describe('AnswerService', () => {
   let service: AnswerService;
@@ -16,7 +17,10 @@ describe('AnswerService', () => {
         AnswerService,
         {
           provide: PrismaService,
-          useValue: {},
+          useValue: {
+            inquiry: {},
+            answer: {},
+          },
         },
       ],
     }).compile();
@@ -27,19 +31,19 @@ describe('AnswerService', () => {
 
   it('getAnswerByIdx success', async () => {
     // 1. get answer with prisma
-    prismaMock.answer.findFirst = jest.fn().mockResolvedValue({
+    prismaMock.answer.findUnique = jest.fn().mockResolvedValue({
       idx: 1,
     });
 
     await expect(service.getAnswerByIdx(1)).resolves.toBeInstanceOf(
       AnswerEntity,
     );
-    expect(prismaMock.answer.findFirst).toHaveBeenCalledTimes(1);
+    expect(prismaMock.answer.findUnique).toHaveBeenCalledTimes(1);
   });
 
   it('getAnswerByIdx fail - not found', async () => {
     // not found
-    prismaMock.answer.findFirst = jest.fn().mockResolvedValue(null);
+    prismaMock.answer.findUnique = jest.fn().mockResolvedValue(null);
 
     await expect(service.getAnswerByIdx(1)).rejects.toThrow(
       AnswerNotFoundException,
@@ -47,10 +51,22 @@ describe('AnswerService', () => {
   });
 
   it('createAnswer success', async () => {
-    // 1. get answer with inquiry idx
-    prismaMock.answer.findFirst = jest.fn().mockResolvedValue(null);
+    // 1. start transaction
+    prismaMock.$transaction = jest
+      .fn()
+      .mockImplementation(
+        async (func = (tx: PrismaService) => Promise<any>) => {
+          return await func(prismaMock);
+        },
+      );
 
-    // 2. create answer
+    // 2. find unqiue inquiry in transaction
+    prismaMock.inquiry.findUnique = jest.fn().mockResolvedValue({
+      idx: 1,
+      Answer: [],
+    });
+
+    // 3. create answer
     prismaMock.answer.create = jest.fn().mockResolvedValue({
       idx: 1,
     });
@@ -58,18 +74,51 @@ describe('AnswerService', () => {
     await expect(service.createAnswer(1, {} as CreateAnswerDto)).resolves.toBe(
       1,
     );
-    expect(prismaMock.answer.findFirst).toHaveBeenCalledTimes(1);
+    expect(prismaMock.$transaction).toHaveBeenCalledTimes(1);
+    expect(prismaMock.inquiry.findUnique).toHaveBeenCalledTimes(1);
     expect(prismaMock.answer.create).toHaveBeenCalledTimes(1);
   });
 
   it('createAnswer fail - inquiry already has answer', async () => {
-    // inquiry already has answer
-    prismaMock.answer.findFirst = jest.fn().mockResolvedValue({
+    prismaMock.$transaction = jest
+      .fn()
+      .mockImplementation(
+        async (func = (tx: PrismaService) => Promise<any>) => {
+          return await func(prismaMock);
+        },
+      );
+
+    // inquiry has already answer
+    prismaMock.inquiry.findUnique = jest.fn().mockResolvedValue({
       idx: 1,
+      Answer: [
+        {
+          idx: 2,
+          contents: '',
+          inquiryIdx: 1,
+        },
+      ],
     });
 
     await expect(
       service.createAnswer(1, {} as CreateAnswerDto),
     ).rejects.toThrow(ConflictException);
+  });
+
+  it('createAnswer fail - cannot find inquiry', async () => {
+    prismaMock.$transaction = jest
+      .fn()
+      .mockImplementation(
+        async (func = (tx: PrismaService) => Promise<any>) => {
+          return await func(prismaMock);
+        },
+      );
+
+    // inquiry has already answer
+    prismaMock.inquiry.findUnique = jest.fn().mockResolvedValue(null);
+
+    await expect(
+      service.createAnswer(1, {} as CreateAnswerDto),
+    ).rejects.toThrow(InquiryNotFoundException);
   });
 });
