b0b8dea469760fca3867e1f13bac27bcf400ba05
jochongs <joch2712@naver.com>
2024-03-04 10:53:13 +0900

    feat: add decorator in createBannerDto

--- src/api/banner/dto/CreateBannerDto.ts
+++ src/api/banner/dto/CreateBannerDto.ts
@@ -1,7 +1,14 @@
+import { IsString, Length, ValidateNested } from 'class-validator';
 import { UploadFileDto } from '../../upload/dto/UploadFileDto';
 
 export class CreateBannerDto {
+  @Length(1, 100)
   name: string;
+
+  @IsString()
+  @Length(1, 2000)
   link: string;
+
+  @ValidateNested()
   img: UploadFileDto;
 }
