ce7dcefb85653ed485f6c688bf78c6a52222bc04
jochongs <joch2712@naver.com>
2024-04-23 05:12:40 +0900

    fix: Fix LiketModule DI

--- src/api/liket/liket.module.ts
+++ src/api/liket/liket.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { LiketService } from './liket.service';
 import { LiketController } from './liket.controller';
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UploadModule],
   controllers: [LiketController],
   providers: [LiketService],
 })
