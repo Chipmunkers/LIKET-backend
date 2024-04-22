86de41c11dcfb20af1cb6c4d2588eb6e49785195
jochongs <joch2712@naver.com>
2024-04-23 04:36:48 +0900

    refactor: Delete unnecessary file

--- src/api/review/dto/response/GetReviewAllForAdminResponseDto.ts
+++
@@ -1,9 +0,0 @@
-import { ValidateNested } from 'class-validator';
-import { ReviewEntity } from '../../entity/ReviewEntity';
-
-export class GetReviewAllForAdminResponseDto {
-  @ValidateNested()
-  reviewList: ReviewEntity<'summary'>[];
-
-  count: number;
-}
