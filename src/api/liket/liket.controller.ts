b9525a8463af45371a1ea7124753372c9e2248d3
jochongs <joch2712@naver.com>
2024-04-23 05:16:41 +0900

    feat: Add GET /liket/:idx API

--- src/api/liket/liket.controller.ts
+++ src/api/liket/liket.controller.ts
@@ -1,6 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  ForbiddenException,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { LiketService } from './liket.service';
+import { LiketEntity } from './entity/LiketEntity';
+import { LoginAuthGuard } from '../../common/guard/auth.guard';
+import { TypedException } from '@nestia/core';
+import { ExceptionDto } from '../../common/dto/ExceptionDto';
+import { User } from '../../common/decorator/user.decorator';
+import { LoginUserDto } from '../../common/dto/LoginUserDto';
 
 @Controller('liket')
 export class LiketController {
-  constructor() {}
+  constructor(private readonly liketService: LiketService) {}
+
+  /**
+   * Get LIKET by idx API
+   * @summary Get LIKET by idx API
+   *
+   * @tag LIKET
+   */
+  @Get('/:idx')
+  @HttpCode(200)
+  @TypedException<ExceptionDto>(400, 'Invalid path parameter')
+  @TypedException<ExceptionDto>(401, 'No token or invalid token')
+  @TypedException<ExceptionDto>(403, 'Suspended user')
+  @TypedException<ExceptionDto>(404, 'Cannot find LIKET')
+  @TypedException<ExceptionDto>(500, 'Server Error')
+  @UseGuards(LoginAuthGuard)
+  public async getLiketByIdx(
+    @Param('idx', ParseIntPipe) idx: number,
+    @User() loginUser: LoginUserDto,
+  ): Promise<LiketEntity<'detail'>> {
+    const liket = await this.liketService.getLiketByIdx(idx);
+
+    if (liket.author.idx !== loginUser.idx) {
+      throw new ForbiddenException('Permission denied');
+    }
+
+    return liket;
+  }
 }
