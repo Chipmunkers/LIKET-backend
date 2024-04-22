66b5c1e412b3a538b47912f5d774b6cb7353a51e
jochongs <joch2712@naver.com>
2024-04-23 05:19:24 +0900

    feat: Add PUT /liket/:idx API

--- src/api/liket/liket.controller.ts
+++ src/api/liket/liket.controller.ts
@@ -1,10 +1,12 @@
 import {
+  Body,
   Controller,
   ForbiddenException,
   Get,
   HttpCode,
   Param,
   ParseIntPipe,
+  Put,
   UseGuards,
 } from '@nestjs/common';
 import { LiketService } from './liket.service';
@@ -14,6 +16,7 @@ import { TypedException } from '@nestia/core';
 import { ExceptionDto } from '../../common/dto/ExceptionDto';
 import { User } from '../../common/decorator/user.decorator';
 import { LoginUserDto } from '../../common/dto/LoginUserDto';
+import { UpdateLiketDto } from './dto/UpdateLiketDto';
 
 @Controller('liket')
 export class LiketController {
@@ -29,7 +32,7 @@ export class LiketController {
   @HttpCode(200)
   @TypedException<ExceptionDto>(400, 'Invalid path parameter')
   @TypedException<ExceptionDto>(401, 'No token or invalid token')
-  @TypedException<ExceptionDto>(403, 'Suspended user')
+  @TypedException<ExceptionDto>(403, 'Permission denied')
   @TypedException<ExceptionDto>(404, 'Cannot find LIKET')
   @TypedException<ExceptionDto>(500, 'Server Error')
   @UseGuards(LoginAuthGuard)
@@ -44,5 +47,35 @@ export class LiketController {
     }
 
     return liket;
+  }
+
+  /**
+   * Update LIKET by idx API
+   * @summary Update LIKET by idx API
+   *
+   * @tag LIKET
+   */
+  @Put('/:idx')
+  @HttpCode(201)
+  @TypedException<ExceptionDto>(400, 'Invalid path parameter or body')
+  @TypedException<ExceptionDto>(401, 'No token or invalid token')
+  @TypedException<ExceptionDto>(403, 'Permission denied')
+  @TypedException<ExceptionDto>(404, 'Cannot find LIKET')
+  @TypedException<ExceptionDto>(500, 'Server Error')
+  @UseGuards(LoginAuthGuard)
+  public async updateLiketByIdx(
+    @Param('idx', ParseIntPipe) idx: number,
+    @User() loginUser: LoginUserDto,
+    @Body() updateDto: UpdateLiketDto,
+  ) {
+    const liket = await this.liketService.getLiketByIdx(idx);
+
+    if (liket.author.idx !== loginUser.idx) {
+      throw new ForbiddenException('Permission denied');
+    }
+
+    await this.liketService.updateLiketByIdx(idx, updateDto);
+
+    return;
   }
 }
