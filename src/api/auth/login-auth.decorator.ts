f6f6536ec0b650cda8a8964f12c331029bb20301
jochongs <joch2712@naver.com>
2024-06-10 21:23:36 +0900

    hot-fix: Add ApiBearerAuth decorator in LoginAuth

--- src/api/auth/login-auth.decorator.ts
+++ src/api/auth/login-auth.decorator.ts
@@ -1,10 +1,12 @@
 import { UseGuards, applyDecorators } from '@nestjs/common';
 import { Exception } from '../../common/decorator/exception.decorator';
 import { LoginAuthGuard } from './auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 export const LoginAuth = () => {
   return applyDecorators(
     UseGuards(LoginAuthGuard),
+    ApiBearerAuth('token'),
     Exception(401, 'No token or invalid token'),
     Exception(403, 'Permission denied'),
   );
