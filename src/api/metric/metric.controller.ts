fea0cc001bc54c61e530f32f3ca66ff8afdcd1b5
jochongs <joch2712@naver.com>
2024-07-29 23:00:12 +0900

    hot-fix: Add logger in metric controller

--- src/api/metric/metric.controller.ts
+++ src/api/metric/metric.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PrismaService } from '../../common/module/prisma/prisma.service';
+import { Logger } from '../../common/module/logger/logger.decorator';
+import { LoggerService } from '../../common/module/logger/logger.service';
 
 @Controller('/metric')
 export class MetricController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    @Logger(MetricController.name) private readonly logger: LoggerService,
+  ) {}
 
   @Get()
   async getMetricAll() {
+    this.logger.log(this.getMetricAll, 'GET /metric');
+
     const metric = await this.prisma.$metrics.prometheus();
 
     return metric;
