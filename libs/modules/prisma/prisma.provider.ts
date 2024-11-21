7bd94eae1edea9540491955fa2f9e5a9c673a0dc
jochongs <joch2712@naver.com>
2024-11-21 17:36:56 +0900

    feat: Add prisma module to libs

---
+++ libs/modules/prisma/prisma.provider.ts
@@ -0,0 +1,20 @@
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+/**
+ * Prisma Client
+ *
+ * @author jochongs
+ */
+export class PrismaProvider
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
