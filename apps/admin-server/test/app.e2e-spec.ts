fd8f3f4df74c81d11829bad44182270e1a8197f8
jochongs <joch2712@naver.com>
2024-11-24 15:22:06 +0900

    fix: Fix wrong import in test-e2e

--- apps/admin-server/test/app.e2e-spec.ts
+++ apps/admin-server/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AdminServerModule } from './../src/admin-server.module';
+import { AppModule } from './../src/app.module';
 
 describe('AdminServerController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AdminServerModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
