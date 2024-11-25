e1342e3b3790f1a9f6206d3beac7168d7a79ba2a
jochongs <joch2712@naver.com>
2024-11-25 18:04:50 +0900

    test: Apply test helper to inquiry test

--- apps/user-server/test/e2e/api/inquiry/inquiry.e2e-spec.ts
+++ apps/user-server/test/e2e/api/inquiry/inquiry.e2e-spec.ts
@@ -6,41 +6,24 @@ import { AppModule } from '../../../../src/app.module';
 import { PrismaService } from '../../../../src/common/module/prisma/prisma.service';
 import { AppGlobalSetting } from '../../setup/app-global.setup';
 import * as request from 'supertest';
+import { TestHelper } from '../../setup/test.helper';
 
 describe('Inquiry (e2e)', () => {
-  let app: INestApplication;
-  let appModule: TestingModule;
-  const prismaSetting = PrismaSetting.setup();
-
-  let loginUsers: TestLoginUsers;
+  const test = TestHelper.create();
 
   beforeEach(async () => {
-    await prismaSetting.BEGIN();
-
-    appModule = await Test.createTestingModule({
-      imports: [AppModule],
-    })
-      .overrideProvider(PrismaService)
-      .useValue(prismaSetting.getPrisma())
-      .compile();
-    app = appModule.createNestApplication();
-    AppGlobalSetting.setup(app);
-    await app.init();
-
-    loginUsers = await LoginSetting.setup(loginUsers, app);
+    await test.init();
   });
 
   afterEach(async () => {
-    prismaSetting.ROLLBACK();
-    await appModule.close();
-    await app.close();
+    await test.destroy();
   });
 
   describe('GET /inquiry/all', () => {
     it('Success - user1', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
 
-      const response = await request(app.getHttpServer())
+      const response = await request(test.getServer())
         .get('/inquiry/all')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(200);
@@ -51,9 +34,9 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('Success - user2', async () => {
-      const loginUser = loginUsers.user2;
+      const loginUser = test.getLoginUsers().user2;
 
-      const response = await request(app.getHttpServer())
+      const response = await request(test.getServer())
         .get('/inquiry/all')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(200);
@@ -64,16 +47,16 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('No token', async () => {
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .get('/inquiry/all')
         .set('Authorization', `Bearer ${null}`)
         .expect(401);
     });
 
     it('Invalid DTO - page', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .get('/inquiry/all')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .query({
@@ -85,10 +68,10 @@ describe('Inquiry (e2e)', () => {
 
   describe('GET /inquiry/:idx', () => {
     it('Success', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const idx = 1;
 
-      const response = await request(app.getHttpServer())
+      const response = await request(test.getServer())
         .get(`/inquiry/${idx}`)
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(200);
@@ -98,10 +81,10 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('Non-existent inquiry', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const idx = 9999; // Non-existent inquiry
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .get(`/inquiry/${idx}`)
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(404);
@@ -110,17 +93,17 @@ describe('Inquiry (e2e)', () => {
     it('No token', async () => {
       const idx = 1;
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .get(`/inquiry/${idx}`)
         .set('Authorization', `Bearer ${null}`)
         .expect(401);
     });
 
     it('Permission denied - non author user', async () => {
-      const loginUser = loginUsers.user2;
+      const loginUser = test.getLoginUsers().user2;
       const idx = 1;
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .get(`/inquiry/${idx}`)
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(403);
@@ -129,7 +112,7 @@ describe('Inquiry (e2e)', () => {
 
   describe('POST /inquiry', () => {
     it('Success', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const createDto = {
         title: 'title',
         contents: 'test-contents',
@@ -137,7 +120,7 @@ describe('Inquiry (e2e)', () => {
         typeIdx: 1,
       };
 
-      const response = await request(app.getHttpServer())
+      const response = await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send(createDto)
@@ -147,7 +130,7 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('No token', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const createDto = {
         title: 'title',
         contents: 'test-contents',
@@ -155,7 +138,7 @@ describe('Inquiry (e2e)', () => {
         typeIdx: 1,
       };
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${null}`)
         .send(createDto)
@@ -163,14 +146,14 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('Invalid DTO - title', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const createDto = {
         contents: 'test-contents',
         imgList: [],
         typeIdx: 1,
       };
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send(createDto)
@@ -178,14 +161,14 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('Invalid DTO - contents', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const createDto = {
         title: 'title',
         imgList: [],
         typeIdx: 1,
       };
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send(createDto)
@@ -193,13 +176,13 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('Invalid DTO - imgList', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const createDto = {
         title: 'title',
         typeIdx: 1,
       };
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send(createDto)
@@ -207,14 +190,14 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('Invalid DTO - imgList', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const createDto = {
         title: 'title',
         contents: 'test-contents',
         typeIdx: 1,
       };
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send(createDto)
@@ -222,14 +205,14 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('Invalid DTO - typeIdx', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const createDto = {
         title: 'title',
         contents: 'test-contents',
         imgList: [],
       };
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send(createDto)
@@ -237,7 +220,7 @@ describe('Inquiry (e2e)', () => {
     });
 
     it('Invalid DTO - no typeIdx', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
       const createDto = {
         title: 'title',
         contents: 'test-contents',
@@ -245,7 +228,7 @@ describe('Inquiry (e2e)', () => {
         typeIdx: 999,
       };
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send(createDto)
@@ -255,10 +238,10 @@ describe('Inquiry (e2e)', () => {
 
   describe('DELETE /inquiry/:idx', () => {
     it('Success', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
 
       // 문의 생성
-      const response = await request(app.getHttpServer())
+      const response = await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send({
@@ -271,44 +254,44 @@ describe('Inquiry (e2e)', () => {
       expect(response.body?.idx).toBeDefined();
       const createdInquiry = response.body;
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .delete(`/inquiry/${createdInquiry.idx}`)
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(201);
     });
 
     it('No token', async () => {
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .delete(`/inquiry/1`)
         .set('Authorization', `Bearer ${null}`)
         .expect(401);
     });
 
     it('Permission denied - non author user', async () => {
-      const loginUser = loginUsers.user2;
+      const loginUser = test.getLoginUsers().user2;
       const idx = 1;
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .delete(`/inquiry/${idx}`)
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(403);
     });
 
     it('Non-existent inquiry', async () => {
-      const loginUser = loginUsers.user2;
+      const loginUser = test.getLoginUsers().user2;
       const idx = 999999; // Non-existent inquiry
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .delete(`/inquiry/${idx}`)
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(404);
     });
 
     it('Delete twice', async () => {
-      const loginUser = loginUsers.user1;
+      const loginUser = test.getLoginUsers().user1;
 
       // 문의 생성
-      const response = await request(app.getHttpServer())
+      const response = await request(test.getServer())
         .post('/inquiry')
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .send({
@@ -321,12 +304,12 @@ describe('Inquiry (e2e)', () => {
       expect(response.body?.idx).toBeDefined();
       const createdInquiry = response.body;
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .delete(`/inquiry/${createdInquiry.idx}`)
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(201);
 
-      await request(app.getHttpServer())
+      await request(test.getServer())
         .delete(`/inquiry/${createdInquiry.idx}`)
         .set('Authorization', `Bearer ${loginUser.accessToken}`)
         .expect(404);
