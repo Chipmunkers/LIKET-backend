93944cdccecdca2f00938041e7c37f78fdc8a216
jochongs <joch2712@naver.com>
2024-11-27 13:55:54 +0900

    chore(user-server): Add code comment in metric domain files

--- apps/user-server/src/api/metric/metric.controller.ts
+++ apps/user-server/src/api/metric/metric.controller.ts
@@ -5,6 +5,11 @@ import { PrismaProvider } from 'libs/modules';
 export class MetricController {
   constructor(private readonly prisma: PrismaProvider) {}
 
+  /**
+   * 메트릭 지표 수입을 위한 메서드
+   *
+   * @author jochongs
+   */
   @Get()
   async getMetricAll() {
     const metric = await this.prisma.$metrics.prometheus();
