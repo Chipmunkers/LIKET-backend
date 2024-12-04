89d6750bbe076e0b9fda790642f8b7b73a7bfed7
jochongs <joch2712@naver.com>
2024-12-04 14:35:18 +0900

    chore(batch-server): Add PERFORM_STATE

---
+++ apps/batch-server/src/kopis-content/type/PERFORM_STATE.ts
@@ -0,0 +1,21 @@
+/**
+ * 공연 상태 코드
+ *
+ * @author jochongs
+ */
+export const PERFORM_STATE = {
+  /**
+   * 공연예정
+   */
+  UPCOMING: '01',
+
+  /**
+   * 공연중
+   */
+  ONGOING: '02',
+
+  /**
+   * 공연완료
+   */
+  COMPLETED: '03',
+} as const;
