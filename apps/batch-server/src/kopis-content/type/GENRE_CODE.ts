ee0db7922e008eb9afb2b91bbdb7c9699753227f
jochongs <joch2712@naver.com>
2024-12-04 14:33:47 +0900

    chore(batch-server): Add GENRE_CODE

---
+++ apps/batch-server/src/kopis-content/type/GENRE_CODE.ts
@@ -0,0 +1,51 @@
+/**
+ * KOPIS 공연예술 API 장르 코드 목록
+ *
+ * @author jochongs
+ */
+export const GENRE_CODE = {
+  /**
+   * 연극
+   */
+  THEATER: 'AAAA',
+
+  /**
+   * 무용
+   */
+  DANCE: 'BBBC',
+
+  /**
+   * 대중무용
+   */
+  POPULAR_DANCE: 'BBBE',
+
+  /**
+   * 서양음악
+   */
+  WESTERN_MUSIC: 'CCCA',
+
+  /**
+   * 한국음악
+   */
+  KOREAN_MUSIC: 'CCCC',
+
+  /**
+   * 대중음악
+   */
+  POPULAR_MUSIC: 'CCCD',
+
+  /**
+   * 복합
+   */
+  COMPLEX: 'EEEA',
+
+  /**
+   * 서커스/마술
+   */
+  CIRCUS_MAGIC: 'EEEB',
+
+  /**
+   * 뮤지컬
+   */
+  MUSICAL: 'GGGA',
+} as const;
