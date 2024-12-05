57b44bff207b7a3f4abb30b19ae51adbd2d89824
jochongs <joch2712@naver.com>
2024-12-05 17:02:28 +0900

    chore(batch-server): Add code comment

--- apps/batch-server/src/kopis-perform/entity/summary-perform.entity.ts
+++ apps/batch-server/src/kopis-perform/entity/summary-perform.entity.ts
@@ -1,6 +1,11 @@
 import { PickType } from '@nestjs/swagger';
 import { PerformEntity } from './perform.entity';
 
+/**
+ * 목록보기 전용 Perform Entity
+ *
+ * @author jochongs
+ */
 export class SummaryPerformEntity extends PickType(PerformEntity, [
   'mt20id',
   'prfnm',
--- apps/batch-server/src/kopis-perform/exception/KeyLimitExceedException.ts
+++ apps/batch-server/src/kopis-perform/exception/KeyLimitExceedException.ts
@@ -1,3 +1,8 @@
+/**
+ * Service 키 사용 초과 예외
+ *
+ * @author jochongs
+ */
 export class KeyLimitExceedException extends Error {
   constructor(message: string) {
     super(message);
