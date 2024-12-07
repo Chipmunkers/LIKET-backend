ddfc7efe2de888aa18d9622be48b03fb09245051
jochongs <joch2712@naver.com>
2024-12-07 16:26:52 +0900

    feat(batch-server): Implement already exist temp content check logic

--- apps/batch-server/src/temp-content/temp-content.repository.ts
+++ apps/batch-server/src/temp-content/temp-content.repository.ts
@@ -7,12 +7,23 @@ export class TempContentRepository {
   constructor(private readonly prisma: PrismaProvider) {}
 
   /**
+   * @author jochongs
+   */
+  public async selectTempContentById(id: string) {
+    return await this.prisma.tempCultureContent.findUnique({
+      where: {
+        performId: id,
+      },
+    });
+  }
+
+  /**
    * TempContentEntity를 통해 temp content 저장하기
    *
    * @author jochongs
    */
   public async insertTempContent(tempEntity: TempContentEntity) {
-    await this.prisma.$transaction(async (tx) => {
+    return await this.prisma.$transaction(async (tx) => {
       const location = await tx.tempContentLocation.create({
         data: {
           address: tempEntity.location.address,
@@ -26,7 +37,7 @@ export class TempContentRepository {
         },
       });
 
-      await tx.tempCultureContent.create({
+      return await tx.tempCultureContent.create({
         data: {
           locationIdx: location.idx,
           genreIdx: tempEntity.genreIdx,
@@ -43,6 +54,64 @@ export class TempContentRepository {
           isPet: tempEntity.isPet,
           isParking: tempEntity.isParking,
           TempContentImg: {
+            createMany: {
+              data: tempEntity.imgList.map((img) => ({
+                imgPath: img,
+              })),
+            },
+          },
+        },
+      });
+    });
+  }
+
+  /**
+   * TempContentEntity를 Update 하는 메서드
+   *
+   * @author jochongs
+   */
+  public async updateTempContentByIdx(
+    idx: number,
+    locationIdx: number,
+    tempEntity: TempContentEntity,
+  ) {
+    await this.prisma.$transaction(async (tx) => {
+      await tx.tempContentLocation.update({
+        where: {
+          idx: locationIdx,
+        },
+        data: {
+          address: tempEntity.location.address,
+          detailAddress: tempEntity.location.detailAddress,
+          region1Depth: tempEntity.location.region1Depth,
+          region2Depth: tempEntity.location.region2Depth,
+          hCode: tempEntity.location.hCode,
+          bCode: tempEntity.location.bCode,
+          positionX: tempEntity.location.positionX,
+          positionY: tempEntity.location.positionY,
+        },
+      });
+
+      return await tx.tempCultureContent.update({
+        where: {
+          idx,
+        },
+        data: {
+          genreIdx: tempEntity.genreIdx,
+          ageIdx: tempEntity.ageIdx,
+          performId: tempEntity.id,
+          title: tempEntity.title,
+          description: tempEntity.description,
+          websiteLink: tempEntity.websiteLink,
+          startDate: tempEntity.startDate,
+          endDate: tempEntity.endDate,
+          openTime: tempEntity.openTime,
+          isFee: tempEntity.isFee,
+          isReservation: tempEntity.isReservation,
+          isPet: tempEntity.isPet,
+          isParking: tempEntity.isParking,
+          TempContentImg: {
+            deleteMany: {},
             createMany: {
               data: tempEntity.imgList.map((img) => ({
                 imgPath: img,
--- apps/batch-server/src/temp-content/temp-content.schedule.ts
+++ apps/batch-server/src/temp-content/temp-content.schedule.ts
@@ -1,14 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { TempContentService } from './temp-content.service';
 import { Cron } from '@nestjs/schedule';
+import { TempContentPipeService } from './temp-content-pipe.service';
+import { TempContentRepository } from './temp-content.repository';
 
 @Injectable()
 export class TempContentSchedule {
-  constructor(private readonly tempCultureContentService: TempContentService) {}
+  constructor(
+    private readonly tempContentService: TempContentService,
+    private readonly tempContentPipeService: TempContentPipeService,
+    private readonly tempContentRepository: TempContentRepository,
+    private readonly logger: Logger,
+  ) {}
 
   @Cron('1 0 0 * * *')
-  async performList() {
-    const rawTempContentEntity =
-      await this.tempCultureContentService.getDetailPerformAllUpdatedAfterYesterday();
+  async savePerformList() {
+    const rawTempContentEntityList =
+      await this.tempContentService.getDetailPerformAllUpdatedAfterYesterday();
+
+    for (const rawTempContentEntity of rawTempContentEntityList) {
+      try {
+        const tempContentEntity =
+          await this.tempContentPipeService.createTempContentEntity(
+            rawTempContentEntity,
+          );
+
+        const tempContent =
+          await this.tempContentRepository.selectTempContentById(
+            tempContentEntity.id,
+          );
+
+        if (!tempContent) {
+          // 최초 INSERT
+          await this.tempContentRepository.insertTempContent(tempContentEntity);
+          this.logger.debug(
+            'Success saving perform | id = ' + tempContentEntity.id,
+            'perform-cron-job',
+          );
+        } else {
+          // 이미 있는 데이터
+          await this.tempContentRepository.updateTempContentByIdx(
+            tempContent.idx,
+            tempContent.locationIdx,
+            tempContentEntity,
+          );
+          this.logger.debug(
+            'Success update perform | id = ' + tempContentEntity.id,
+            'perform-cron-job',
+          );
+        }
+      } catch (err) {
+        this.logger.error(
+          'Fail to save content entity | id = ' +
+            rawTempContentEntity.perform.mt20id,
+          'perform-cron-job',
+        );
+        console.log(err);
+      }
+    }
   }
 }
