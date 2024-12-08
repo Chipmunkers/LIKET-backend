6674a566efc467122ffc7c28cda854db4d8e7ee0
jochongs <joch2712@naver.com>
2024-12-08 16:36:34 +0900

    hot-fix(batch-server): Fix yesterday logic, main.ts

--- apps/batch-server/src/main.ts
+++ apps/batch-server/src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger } from '@nestjs/common';
-import { TempContentSchedule } from './temp-content/temp-content.schedule';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new ConsoleLogger(),
   });
-
-  const tempContentSchedule = app.get(TempContentSchedule);
-
-  await tempContentSchedule.savePerformListCronJob();
 
   await app.listen(3000);
 }
