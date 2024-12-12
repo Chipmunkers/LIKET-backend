4987505ccab9a8d57a5f6317282ee77614476955
jochongs <joch2712@naver.com>
2024-12-12 16:10:57 +0900

    chore(user-server): Make AppGlobalSetting deprecated

--- apps/user-server/test/e2e/setup/app-global.setup.ts
+++ apps/user-server/test/e2e/setup/app-global.setup.ts
@@ -1,6 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
+/**
+ * @author jochongs
+ *
+ * @deprecated
+ */
 export class AppGlobalSetting {
   static setup(app: INestApplication) {
     app.use(cookieParser(process.env.COOKIE_SECRET));
