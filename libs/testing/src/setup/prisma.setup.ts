9da326dce86d8b291cf8cf0bd9938b1d2d64e799
jochongs <joch2712@naver.com>
2024-12-12 16:05:44 +0900

    chore(common): Add author code comment in prisma setup

--- libs/testing/src/setup/prisma.setup.ts
+++ libs/testing/src/setup/prisma.setup.ts
@@ -1,6 +1,9 @@
 import { PrismaTestingHelper } from '@chax-at/transactional-prisma-testing';
 import { PrismaProvider } from 'libs/modules';
 
+/**
+ * @author jochongs
+ */
 export class PrismaSetup {
   private readonly prisma: PrismaProvider;
   private readonly prismaTestingHelper: PrismaTestingHelper<PrismaProvider>;
