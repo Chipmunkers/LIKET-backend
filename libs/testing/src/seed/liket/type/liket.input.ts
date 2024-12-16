c568c6f95bb8397c0b825f80429ee99e7dc805e0
jochongs <joch2712@naver.com>
2024-12-16 20:52:21 +0900

    chore(test): Add liket input output type for seed

---
+++ libs/testing/src/seed/liket/type/liket.input.ts
@@ -0,0 +1,61 @@
+import { BgImgInfoEntity } from 'apps/user-server/src/api/liket/entity/bgImgInfo.entity';
+import { ImgShapeEntity } from 'apps/user-server/src/api/liket/entity/imgShape.entity';
+
+/**
+ * @author jochongs
+ */
+export type LiketInput = {
+  /**
+   * 리뷰 인덱스
+   */
+  reviewIdx: number;
+
+  /**
+   * 배경 이미지 경로
+   *
+   * @default "랜덤 이미지 경로"
+   */
+  bgImgPath?: string;
+
+  /**
+   * 배경 이미지 정보
+   *
+   * @default "랜덤 정보 json"
+   */
+  bgImgInfo?: BgImgInfoEntity;
+
+  /**
+   * 카드 이미지 경로
+   *
+   * @default "랜덤 이미지 경로"
+   */
+  cardImgPath?: string;
+
+  /**
+   * 글자 json
+   *
+   * @default null
+   */
+  textShape?: BgImgInfoEntity | null;
+
+  /**
+   * 이미지 json 배열
+   *
+   * @default []
+   */
+  ImgShapeEntity?: ImgShapeEntity[];
+
+  /**
+   * 사이즈
+   *
+   * @defualt "0 ~ 127 사이 랜덤 사이즈"
+   */
+  size?: number;
+
+  /**
+   * 설명
+   *
+   * @default "랜덤 문자열"
+   */
+  description?: string;
+};
