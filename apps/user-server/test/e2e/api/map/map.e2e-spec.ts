902dd1859e584a2cfa68f7a4e82be515707e0ebe
jochongs <joch2712@naver.com>
2024-12-17 18:35:22 +0900

    fix(user-server): Fix map culture-content open where clause

--- apps/user-server/test/e2e/api/map/map.e2e-spec.ts
+++ apps/user-server/test/e2e/api/map/map.e2e-spec.ts
@@ -1,9 +1,12 @@
 import * as request from 'supertest';
 import { AppModule } from 'apps/user-server/src/app.module';
 import { TestHelper } from 'apps/user-server/test/e2e/setup/test.helper';
+import { CultureContentSeedHelper } from 'libs/testing';
+import { MapContentEntity } from 'apps/user-server/src/api/map/entity/map-content.entity';
 
 describe('Map (e2e)', () => {
   const test = TestHelper.create(AppModule);
+  const contentSeedHelper = test.seedHelper(CultureContentSeedHelper);
 
   beforeEach(async () => {
     await test.init();
@@ -44,6 +47,74 @@ describe('Map (e2e)', () => {
           'bottom-y': 29,
         })
         .expect(200);
+    });
+
+    it('Success: test with not open contents', async () => {
+      const oneMonthAgo = new Date();
+      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+
+      const oneMonthAfter = new Date();
+      oneMonthAfter.setMonth(oneMonthAfter.getMonth() + 1);
+
+      const [firstContents, secondContents, thirdContents] =
+        await contentSeedHelper.seedAll([
+          {
+            userIdx: test.getLoginUsers().user1.idx,
+            acceptedAt: new Date(),
+            startDate: oneMonthAgo,
+            endDate: oneMonthAfter,
+            location: {
+              positionX: 127.5,
+              positionY: 29.5,
+            },
+          },
+          {
+            userIdx: test.getLoginUsers().user1.idx,
+            acceptedAt: new Date(),
+            startDate: oneMonthAgo,
+            endDate: null,
+            location: {
+              positionX: 127.5,
+              positionY: 29.5,
+            },
+          },
+          {
+            // not open contents
+            userIdx: test.getLoginUsers().user1.idx,
+            acceptedAt: new Date(),
+            startDate: oneMonthAfter,
+            endDate: null,
+            location: {
+              positionX: 127.5,
+              positionY: 29.5,
+            },
+          },
+        ]);
+
+      const response = await request(test.getServer())
+        .get(`/map/culture-content/all`)
+        .query({
+          'top-x': 127,
+          'top-y': 30,
+          'bottom-x': 128,
+          'bottom-y': 29,
+        })
+        .expect(200);
+
+      const { contentList }: { contentList: MapContentEntity[] } =
+        response.body;
+
+      expect(contentList.length).toBe(2);
+      expect(
+        contentList
+          .map((content: MapContentEntity) => content.idx)
+          .includes(firstContents.idx),
+      );
+      expect(
+        contentList
+          .map((content: MapContentEntity) => content.idx)
+          .includes(secondContents.idx),
+      );
     });
   });
 
