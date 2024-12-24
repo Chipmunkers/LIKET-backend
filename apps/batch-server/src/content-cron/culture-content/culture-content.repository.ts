71bee7922a25f646a8cf20fbdb0a152b56312b79
jochongs <joch2712@naver.com>
2024-12-24 16:40:59 +0900

    feat(batch-server): Implement insertCultureContent method

--- apps/batch-server/src/content-cron/culture-content/culture-content.repository.ts
+++ apps/batch-server/src/content-cron/culture-content/culture-content.repository.ts
@@ -7,48 +7,57 @@ import { CultureContent } from '@prisma/client';
 export class CultureContentRepository {
   constructor(private readonly prisma: PrismaProvider) {}
 
-  // public async insertCultureContent(
-  //   tempContent: TempContentEntity,
-  // ): Promise<CultureContent> {
-  //   return await this.prisma.$transaction(async (tx) => {
-  //     const location = await tx.location.create({
-  //       data: {
-  //         address: tempContent.location.address,
-  //         detailAddress: tempContent.location.detailAddress,
-  //         region1Depth: tempContent.location.region1Depth,
-  //         region2Depth: tempContent.location.region2Depth,
-  //         hCode: tempContent.location.hCode,
-  //         bCode: tempContent.location.bCode,
-  //         positionX: tempContent.location.positionX,
-  //         positionY: tempContent.location.positionY,
-  //       },
-  //     });
+  public async insertCultureContent(
+    tempContent: TempContentEntity,
+  ): Promise<CultureContent> {
+    return await this.prisma.$transaction(async (tx) => {
+      const location = await tx.location.create({
+        data: {
+          address: tempContent.location.address,
+          detailAddress: tempContent.location.detailAddress,
+          region1Depth: tempContent.location.region1Depth,
+          region2Depth: tempContent.location.region2Depth,
+          hCode: tempContent.location.hCode,
+          bCode: tempContent.location.bCode,
+          positionX: tempContent.location.positionX,
+          positionY: tempContent.location.positionY,
+          sidoCode: tempContent.location.bCode.substring(0, 2),
+          sggCode: tempContent.location.bCode.substring(2, 5),
+          legCode: tempContent.location.bCode.substring(5, 8),
+          riCode: tempContent.location.bCode.substring(8, 10),
+        },
+      });
 
-  //     return await tx.cultureContent.create({
-  //       data: {
-  //         locationIdx: location.idx,
-  //         genreIdx: tempContent.genreIdx,
-  //         ageIdx: tempContent.ageIdx,
-  //         performId: tempContent.id,
-  //         title: tempContent.title,
-  //         description: tempContent.description,
-  //         websiteLink: tempContent.websiteLink,
-  //         startDate: tempContent.startDate,
-  //         endDate: tempContent.endDate,
-  //         openTime: tempContent.openTime,
-  //         isFee: tempContent.isFee,
-  //         isReservation: tempContent.isReservation,
-  //         isPet: tempContent.isPet,
-  //         isParking: tempContent.isParking,
-  //         ContentImg: {
-  //           createMany: {
-  //             data: tempContent.imgList.map((img) => ({
-  //               imgPath: img,
-  //             })),
-  //           },
-  //         },
-  //       },
-  //     });
-  //   });
-  // }
+      return await tx.cultureContent.create({
+        data: {
+          locationIdx: location.idx,
+          genreIdx: tempContent.genreIdx,
+          ageIdx: tempContent.ageIdx,
+          performId: tempContent.id,
+          title: tempContent.title,
+          description: tempContent.description,
+          websiteLink: tempContent.websiteLink,
+          startDate: tempContent.startDate,
+          endDate: tempContent.endDate,
+          openTime: tempContent.openTime,
+          isFee: tempContent.isFee,
+          isReservation: tempContent.isReservation,
+          isPet: tempContent.isPet,
+          isParking: tempContent.isParking,
+          Style: {
+            createMany: {
+              data: tempContent.styleIdxList.map((styleIdx) => ({ styleIdx })),
+            },
+          },
+          ContentImg: {
+            createMany: {
+              data: tempContent.imgList.map((img) => ({
+                imgPath: img,
+              })),
+            },
+          },
+        },
+      });
+    });
+  }
 }
