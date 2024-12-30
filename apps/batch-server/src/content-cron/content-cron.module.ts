7aa73d3f8fa5cf34400d5d4cf8b48a87ea516a59
jochongs <joch2712@naver.com>
2024-12-30 17:08:32 +0900

    hot-fix(batch-server): Fix content cron bug

--- apps/batch-server/src/content-cron/content-cron.module.ts
+++ apps/batch-server/src/content-cron/content-cron.module.ts
@@ -1,11 +1,12 @@
 import { Logger, Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ContentCronSchedule } from 'apps/batch-server/src/content-cron/content-cron.schedule';
 import { ContentCronService } from 'apps/batch-server/src/content-cron/content-cron.service';
 import { CultureContentModule } from 'apps/batch-server/src/content-cron/culture-content/culture-content.module';
 import { KopisModule } from 'apps/batch-server/src/content-cron/external-apis/kopis/kopis.module';
 
 @Module({
-  imports: [KopisModule, CultureContentModule],
+  imports: [KopisModule, CultureContentModule, ScheduleModule.forRoot()],
   providers: [ContentCronService, ContentCronSchedule, Logger],
 })
 export class ContentCronModule {}
