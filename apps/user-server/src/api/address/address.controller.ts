1fa80f8d5208487a13aa4e1ac278d6f7a72ccfb1
jochongs <joch2712@naver.com>
2025-01-13 21:42:46 +0900

    chore(user-server): Update user-server api swagger documents

--- apps/user-server/src/api/address/address.controller.ts
+++ apps/user-server/src/api/address/address.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AddressService } from 'apps/user-server/src/api/address/address.service';
 import { SearchAddressDto } from 'apps/user-server/src/api/address/dto/request/search-address.dto';
 import { LoginAuth } from 'apps/user-server/src/api/auth/login-auth.decorator';
+import { Exception } from 'apps/user-server/src/common/decorator/exception.decorator';
 import { KeywordSearchResultEntity } from 'libs/modules/kakao-address/entity/keyword-search-result.entity';
 
 @Controller('/address')
+@ApiTags('Address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
@@ -16,6 +19,7 @@ export class AddressController {
    * @author jochongs
    */
   @LoginAuth()
+  @Exception(400, 'invalid querystring')
   @Get('/keyword-search/all')
   async searchAddress(
     @Query() dto: SearchAddressDto,
