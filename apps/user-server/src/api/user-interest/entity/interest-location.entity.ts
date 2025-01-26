d55ce81d8d13dfd47a2333ad41d9a30e2acadcb3
jochongs <joch2712@naver.com>
2025-01-26 18:00:09 +0900

    feat(user-server): Implement InterestLocationEntity

---
+++ apps/user-server/src/api/user-interest/entity/interest-location.entity.ts
@@ -0,0 +1,21 @@
+import { InterestLocation } from '@prisma/client';
+
+/**
+ * @author jochongs
+ */
+export class InterestLocationEntity {
+  /**
+   * 법정동 코드
+   */
+  bCode: string;
+
+  constructor(data: InterestLocationEntity) {
+    Object.assign(this, data);
+  }
+
+  static createEntity(location: InterestLocation): InterestLocationEntity {
+    return new InterestLocationEntity({
+      bCode: location.bCode,
+    });
+  }
+}
