3d8c9c99fa5ab552c77df3727a5a25f89b44ea3f
jochongs <joch2712@naver.com>
2025-01-26 17:55:23 +0900

    feat(user-server): Implement GenreInterestEntity

--- apps/user-server/src/api/user-interest/entity/genre-interest.entity.ts
+++ apps/user-server/src/api/user-interest/entity/genre-interest.entity.ts
@@ -1,1 +1,21 @@
-export class GenreInterestEntity {}
+import { InterestGenre } from '@prisma/client';
+
+/**
+ * @author jochongs
+ */
+export class GenreInterestEntity {
+  /**
+   * 장르 인덱스
+   */
+  genreIdx: number;
+
+  constructor(data: GenreInterestEntity) {
+    Object.assign(this, data);
+  }
+
+  static createEntity(interestGenre: InterestGenre): GenreInterestEntity {
+    return new GenreInterestEntity({
+      genreIdx: interestGenre.genreIdx,
+    });
+  }
+}
