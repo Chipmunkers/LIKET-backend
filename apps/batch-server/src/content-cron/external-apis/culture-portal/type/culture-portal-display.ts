4df3e87ba814ce2a65eb034032f8da0f714bb1f7
jochongs <joch2712@naver.com>
2025-01-30 20:21:06 +0900

    feat(batch-server): Iplement CulturePortalDisplay

--- apps/batch-server/src/content-cron/external-apis/culture-portal/type/culture-portal-display.ts
+++ apps/batch-server/src/content-cron/external-apis/culture-portal/type/culture-portal-display.ts
@@ -4,26 +4,107 @@ import { SummaryCulturePortalDisplay } from 'apps/batch-server/src/content-cron/
 /**
  * @author jochongs
  */
-export class CulturePortalDisplay extends PickType(
-  SummaryCulturePortalDisplay,
-  [
-    'seq',
-    'title',
-    'startDate',
-    'endDate',
-    'place',
-    'realmName',
-    'area',
-    'gpsX',
-    'gpsY',
-  ],
-) {
-  content1: string;
-  price: '무료' | string;
+export class CulturePortalDisplay {
+  /**
+   * 공연 일련번호
+   *
+   * @example 274673
+   */
+  seq: string;
+
+  /**
+   * 장소
+   *
+   * @example "오산시립미술관"
+   */
+  place: string | null;
+
+  /**
+   * 제목
+   *
+   * @example 2024 야외컨테이너전 SHOW CON
+   */
+  title: string;
+
+  /**
+   * 시작일
+   *
+   * @example "20240319"
+   */
+  startDate: string;
+
+  /**
+   * 종료일
+   *
+   * @example "20250112"
+   */
+  endDate: string | null;
+
+  /**
+   * 분류명
+   *
+   * @example "미술"
+   */
+  realmName: string;
+
+  /**
+   * 지역명
+   *
+   * @example "경기"
+   */
+  area: string | null;
+
+  /**
+   * x 좌표
+   *
+   * @example "126.95802821008974"
+   */
+  gpsX: string | null;
+
+  /**
+   * y 좌표
+   *
+   * @example "37.51766013367088"
+   */
+  gpsY: string | null;
+
+  /**
+   * 컨텐츠 설명 (HTML 형식이 많이 포함되어있음)
+   */
+  content1: string | null;
+
+  /**
+   * 가격
+   *
+   * !주의: 무료인 경우 "무료" 텍스트가 포함되어있는지 확인해야 함
+   */
+  price: string;
+
+  /**
+   * 전시회 URL
+   */
   url: string | null;
-  imgUrl: string;
+
+  /**
+   * !주의: 이미지 url이 존재하지 않을 수도 있습니다.
+   */
+  imgUrl: string | null;
+
+  /**
+   * 핸드폰 번호
+   */
   phone: string | null;
+
+  /**
+   * 장소 url
+   */
   placeUrl: string | null;
+
+  /**
+   * 장소 주소
+   *
+   * ! 단순 텍스트이기 때문에 믿을 것이 되지 못함.
+   */
   placeAddr: string | null;
   /**
    * 장소 일련 번호
--- apps/batch-server/src/content-cron/external-apis/culture-portal/type/summary-culture-portal-display.ts
+++ apps/batch-server/src/content-cron/external-apis/culture-portal/type/summary-culture-portal-display.ts
@@ -1,7 +1,23 @@
+import { PickType } from '@nestjs/swagger';
+import { CulturePortalDisplay } from 'apps/batch-server/src/content-cron/external-apis/culture-portal/type/culture-portal-display';
+
 /**
  * @author jochongs
  */
-export class SummaryCulturePortalDisplay {
+export class SummaryCulturePortalDisplay extends PickType(
+  CulturePortalDisplay,
+  [
+    'title',
+    'seq',
+    'startDate',
+    'endDate',
+    'place',
+    'realmName',
+    'area',
+    'gpsX',
+    'gpsY',
+  ],
+) {
   /**
    * 공연 전시 목록
    *
@@ -10,72 +26,9 @@ export class SummaryCulturePortalDisplay {
   serviceName: string;
 
   /**
-   * 공연 일련번호
-   *
-   * @example 274673
-   */
-  seq: string;
-
-  /**
-   * 제목
-   *
-   * @example 2024 야외컨테이너전 SHOW CON
-   */
-  title: string;
-
-  /**
-   * 시작일
-   *
-   * @example "20240319"
-   */
-  startDate: string;
-
-  /**
-   * 종료일
-   *
-   * @example "20250112"
-   */
-  endDate: string;
-
-  /**
-   * 장소
-   *
-   * @example "오산시립미술관"
-   */
-  place: string | null;
-
-  /**
-   * 분류명
-   *
-   * @example "미술"
-   */
-  realmName: string;
-
-  /**
-   * 지역명
-   *
-   * @example "경기"
-   */
-  area: string | null;
-
-  /**
    * 썸네일
    *
    * @example "http://www.culture.go.kr/upload/rdf/24/12/rdf_2024120621245354914.jpg"
    */
   thumbnail: string;
-
-  /**
-   * x 좌표
-   *
-   * @example "126.95802821008974"
-   */
-  gpsX: string | null;
-
-  /**
-   * y 좌표
-   *
-   * @example "37.51766013367088"
-   */
-  gpsY: string | null;
 }
