c4bbf638a1170dda8b61f911a538dc94cd67f786
jochongs <joch2712@naver.com>
2025-02-12 19:28:13 +0900

    feat(core): Add culture content model

--- libs/core/src/culture-content/model/culture-content.model.ts
+++ libs/core/src/culture-content/model/culture-content.model.ts
@@ -1,6 +1,9 @@
 import { CultureContentAuthorModel } from 'libs/core/culture-content/model/culture-content-author.model';
 import { CultureContentLocationModel } from 'libs/core/culture-content/model/culture-content-location.model';
+import { CultureContentSelectField } from 'libs/core/culture-content/model/prisma/culture-content-select-field';
+import { AgeModel } from 'libs/core/tag-root/age/model/age.model';
 import { GenreModel } from 'libs/core/tag-root/genre/model/genre.model';
+import { StyleModel } from 'libs/core/tag-root/style/model/style.model';
 
 /**
  * @author jochongs
@@ -17,6 +20,16 @@ export class CultureContentModel {
   genre: GenreModel;
 
   /**
+   * 연령대
+   */
+  age: AgeModel;
+
+  /**
+   * 스타일 목록
+   */
+  styleList: StyleModel[];
+
+  /**
    * 작성자
    */
   author: CultureContentAuthorModel;
@@ -26,5 +39,112 @@ export class CultureContentModel {
    */
   location: CultureContentLocationModel;
 
-  // TODO: 나머지 필드 작성해야함
+  /**
+   * 컨텐츠 ID
+   */
+  id: string | null;
+
+  /**
+   * 컨텐츠 제목
+   */
+  title: string;
+
+  /**
+   * 컨텐츠 설명
+   */
+  description: string | null;
+
+  /**
+   * 관련 웹사이트 링크
+   */
+  websiteLink: string | null;
+
+  /**
+   * 컨텐츠 시작 날짜
+   */
+  startDate: Date;
+
+  /**
+   * 컨텐츠 종료 날짜
+   */
+  endDate: Date | null;
+
+  /**
+   * 조회수
+   */
+  viewCount: number;
+
+  /**
+   * 오픈 시간
+   */
+  openTime: string | null;
+
+  /**
+   * 요금 여부
+   */
+  isFee: boolean;
+
+  /**
+   * 예약 여부
+   */
+  isReservation: boolean;
+
+  /**
+   * 반려동물 출입 가능 여부
+   */
+  isPet: boolean;
+
+  /**
+   * 주차 가능 여부
+   */
+  isParking: boolean;
+
+  /**
+   * 좋아요 수
+   */
+  likeCount: number;
+
+  /**
+   * 생성일
+   */
+  createdAt: Date;
+
+  /**
+   * 활성화 된 시간
+   */
+  acceptedAt: Date | null;
+
+  constructor(data: CultureContentModel) {
+    Object.assign(this, data);
+  }
+
+  public static fromPrisma(
+    content: CultureContentSelectField,
+  ): CultureContentModel {
+    return new CultureContentModel({
+      idx: content.idx,
+      genre: GenreModel.fromPrisma(content.Genre),
+      age: AgeModel.fromPrisma(content.Age),
+      styleList: content.Style.map((style) =>
+        StyleModel.fromPrisma(style.Style),
+      ),
+      author: CultureContentAuthorModel.fromPrisma(content.User),
+      location: CultureContentLocationModel.fromPrisma(content.Location),
+      id: content.id,
+      title: content.title,
+      description: content.description,
+      websiteLink: content.websiteLink,
+      startDate: content.startDate,
+      endDate: content.endDate,
+      viewCount: content.viewCount,
+      openTime: content.openTime,
+      isFee: content.isFee,
+      isReservation: content.isReservation,
+      isPet: content.isPet,
+      isParking: content.isParking,
+      likeCount: content.likeCount,
+      createdAt: content.createdAt,
+      acceptedAt: content.acceptedAt,
+    });
+  }
 }
---
+++ libs/core/src/culture-content/model/prisma/culture-content-select-field.ts
@@ -0,0 +1,81 @@
+import { Prisma } from '@prisma/client';
+
+/**
+ * @author jochongs
+ */
+const CULTURE_CONTENT_SELECT_FIELD =
+  Prisma.validator<Prisma.CultureContentDefaultArgs>()({
+    select: {
+      idx: true,
+      id: true,
+      title: true,
+      description: true,
+      websiteLink: true,
+      startDate: true,
+      endDate: true,
+      viewCount: true,
+      openTime: true,
+      isFee: true,
+      isReservation: true,
+      isPet: true,
+      isParking: true,
+      likeCount: true,
+      createdAt: true,
+      acceptedAt: true,
+      Location: {
+        select: {
+          idx: true,
+          address: true,
+          detailAddress: true,
+          region1Depth: true,
+          region2Depth: true,
+          hCode: true,
+          bCode: true,
+          positionX: true,
+          positionY: true,
+          sidoCode: true,
+          sggCode: true,
+          legCode: true,
+          riCode: true,
+        },
+      },
+      Genre: {
+        select: {
+          idx: true,
+          name: true,
+          createdAt: true,
+        },
+      },
+      Style: {
+        select: {
+          Style: {
+            select: {
+              idx: true,
+              name: true,
+              createdAt: true,
+            },
+          },
+        },
+      },
+      Age: {
+        select: {
+          idx: true,
+          name: true,
+          createdAt: true,
+        },
+      },
+      User: {
+        select: {
+          idx: true,
+          nickname: true,
+          email: true,
+          profileImgPath: true,
+          isAdmin: true,
+        },
+      },
+    },
+  });
+
+export type CultureContentSelectField = Prisma.CultureContentGetPayload<
+  typeof CULTURE_CONTENT_SELECT_FIELD
+>;
