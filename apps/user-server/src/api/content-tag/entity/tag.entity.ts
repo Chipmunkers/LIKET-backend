11d661f5d0cd3e4582980ca8dfe5a4f0d43c9b6c
jochongs <joch2712@naver.com>
2025-02-14 21:01:12 +0900

    feat(user-server): Implement fromModel static method to entities about culture content

--- apps/user-server/src/api/content-tag/entity/tag.entity.ts
+++ apps/user-server/src/api/content-tag/entity/tag.entity.ts
@@ -4,6 +4,9 @@ import {
   SelectGenreFieldPrisma,
   SelectStyleFieldPrisma,
 } from 'apps/user-server/src/api/content-tag/entity/prisma/select-tag-field';
+import { AgeModel } from 'libs/core/tag-root/age/model/age.model';
+import { GenreModel } from 'libs/core/tag-root/genre/model/genre.model';
+import { StyleModel } from 'libs/core/tag-root/style/model/style.model';
 
 /**
  * @author jochongs
@@ -27,12 +30,23 @@ export class TagEntity {
     Object.assign(this, data);
   }
 
+  /**
+   * `CultureContentCoreModule`이 만들어짐에따라 deprecated 되었습니다.
+   * 대신 `fromModel` 정적 메서드를 사용하십시오.
+   */
   static createEntity(
     data:
       | SelectAgeFieldPrisma
       | SelectGenreFieldPrisma
       | SelectStyleFieldPrisma,
   ): TagEntity {
+    return new TagEntity({
+      idx: data.idx,
+      name: data.name,
+    });
+  }
+
+  public static fromModel(data: StyleModel | GenreModel | AgeModel): TagEntity {
     return new TagEntity({
       idx: data.idx,
       name: data.name,
