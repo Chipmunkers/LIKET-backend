87a722cdd36506fe71be19a6fa92c88168b26dc1
jochongs <joch2712@naver.com>
2025-02-15 18:50:32 +0900

    feat(core): Implement findCultureContentByIdx core method

--- libs/core/src/culture-content/culture-content-core.service.ts
+++ libs/core/src/culture-content/culture-content-core.service.ts
@@ -1,6 +1,33 @@
+import { Transactional } from '@nestjs-cls/transactional';
 import { Injectable } from '@nestjs/common';
+import { CultureContentCoreRepository } from 'libs/core/culture-content/culture-content-core.repository';
+import { CultureContentModel } from 'libs/core/culture-content/model/culture-content.model';
 
 @Injectable()
 export class CultureContentCoreService {
-  constructor() {}
+  constructor(
+    private readonly cultureContentCoreRepository: CultureContentCoreRepository,
+  ) {}
+
+  /**
+   * idx를 통해 문화생활컨텐츠를 탐색하는 메서드
+   *
+   * @author jochongs
+   *
+   * @param idx 문화생활컨텐츠 식별자
+   * @param readUser 조회한 사용자 인덱스
+   */
+  @Transactional()
+  public async findCultureContentByIdx(
+    idx: number,
+    readUser: number,
+  ): Promise<CultureContentModel | null> {
+    const content =
+      await this.cultureContentCoreRepository.selectCultureContentByIdx(
+        idx,
+        readUser,
+      );
+
+    return content && CultureContentModel.fromPrisma(content);
+  }
 }
