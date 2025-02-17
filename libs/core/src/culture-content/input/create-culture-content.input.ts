11796c527f2388303191e1bb2146bddb6ad214df
jochongs <joch2712@naver.com>
2025-02-17 21:11:00 +0900

    feat(core): Add culture content create input

---
+++ libs/core/src/culture-content/input/create-culture-content.input.ts
@@ -0,0 +1,50 @@
+import { CreateCultureContentLocationInput } from 'libs/core/culture-content/input/create-culture-content-location.input';
+import { Age } from 'libs/core/tag-root/age/constant/age';
+import { Genre } from 'libs/core/tag-root/genre/constant/genre';
+
+/**
+ * @author jochongs
+ */
+export class CreateCultureContentInput {
+  /** 장르 */
+  public readonly genreIdx: Genre;
+
+  /** 연령대 */
+  public readonly ageIdx: Age;
+
+  /** 장소 */
+  public readonly location: CreateCultureContentLocationInput;
+
+  /** 컨텐츠 아이디 */
+  public readonly id: string;
+
+  /** 제목 */
+  public readonly title: string;
+
+  /** 설명 */
+  public readonly description?: string;
+
+  /** 웹 사이트 링크 */
+  public readonly websiteLink?: string;
+
+  /** 시작 날짜 */
+  public readonly startDate: Date;
+
+  /** 종료 날짜 */
+  public readonly endDate?: Date;
+
+  /** 오픈 날짜 */
+  public readonly openTime?: string;
+
+  /** 요금 여부 */
+  public readonly isFee: boolean;
+
+  /** 예약 필수 여부 */
+  public readonly isReservation: boolean;
+
+  /** 주차장 존재 여부 */
+  public readonly isParking: boolean;
+
+  /** 활성 여부 */
+  public readonly accept: boolean;
+}
