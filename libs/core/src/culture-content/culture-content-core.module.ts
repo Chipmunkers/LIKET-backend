890471b8fb85c4ff8f0d5582eaa9f481a427a242
jochongs <joch2712@naver.com>
2025-02-19 15:29:01 +0900

    feat(core): Implement CultureContentLikeCoreRepository

--- libs/core/src/culture-content/culture-content-core.module.ts
+++ libs/core/src/culture-content/culture-content-core.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CultureContentCoreRepository } from 'libs/core/culture-content/culture-content-core.repository';
 import { CultureContentCoreService } from 'libs/core/culture-content/culture-content-core.service';
+import { CultureContentLikeCoreRepository } from 'libs/core/culture-content/culture-content-like-core.repository';
 
 @Module({
   imports: [],
-  providers: [CultureContentCoreService, CultureContentCoreRepository],
+  providers: [
+    CultureContentCoreService,
+    CultureContentCoreRepository,
+    CultureContentLikeCoreRepository,
+  ],
   exports: [CultureContentCoreService],
 })
 export class CultureContentCoreModule {}
---
+++ libs/core/src/culture-content/culture-content-like-core.repository.ts
@@ -0,0 +1,75 @@
+import { Transactional, TransactionHost } from '@nestjs-cls/transactional';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { PrismaProvider } from 'libs/modules';
+import { Injectable } from '@nestjs/common';
+import { ContentLike } from '@prisma/client';
+
+@Injectable()
+export class CultureContentLikeCoreRepository {
+  constructor(
+    private readonly txHost: TransactionHost<
+      TransactionalAdapterPrisma<PrismaProvider>
+    >,
+  ) {}
+
+  /**
+   * SELECT content_like_tb WHERE content_idx = $1 AND user_idx = $2
+   *
+   * @author jochongs
+   * @param idx 컨텐츠 식별자
+   * @param userIdx 좋아요 누른 사용자 식별자
+   */
+  public async selectCultureContentLike(
+    idx: number,
+    userIdx: number,
+  ): Promise<ContentLike | null> {
+    return await this.txHost.tx.contentLike.findUnique({
+      where: {
+        contentIdx_userIdx: {
+          contentIdx: idx,
+          userIdx,
+        },
+      },
+    });
+  }
+
+  /**
+   * INSERT content_like_tb
+   *
+   * @author jochongs
+   *
+   * @param idx 컨텐츠 식별자
+   * @param userIdx 좋아요 누른 사용자 식별자
+   */
+  public async insertCultureContentLike(
+    idx: number,
+    userIdx: number,
+  ): Promise<void> {
+    await this.txHost.tx.contentLike.create({
+      data: {
+        userIdx,
+        contentIdx: idx,
+      },
+    });
+  }
+
+  /**
+   * UPDATE like_count = like_count + $2 WHERE idx = $1
+   *
+   * @author jochongs
+   *
+   * @param idx 컨텐츠 식별자
+   * @param count 증가할 숫자
+   */
+  public async increaseCultureContentLikeCountByIdx(
+    idx: number,
+    count: number = 1,
+  ): Promise<void> {
+    await this.txHost.tx.cultureContent.update({
+      where: { idx, deletedAt: null },
+      data: {
+        likeCount: { increment: count },
+      },
+    });
+  }
+}
