6aa1866a6c256468c50b527c97b326155c2c74b5
jochongs <joch2712@naver.com>
2025-02-20 14:08:46 +0900

    chore(core): Add service dependecy to culture content core module

--- libs/core/src/culture-content/culture-content-core.module.ts
+++ libs/core/src/culture-content/culture-content-core.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CultureContentCoreRepository } from 'libs/core/culture-content/culture-content-core.repository';
 import { CultureContentCoreService } from 'libs/core/culture-content/culture-content-core.service';
 import { CultureContentLikeCoreRepository } from 'libs/core/culture-content/culture-content-like-core.repository';
+import { CultureContentLikeCoreService } from 'libs/core/culture-content/culture-content-like-core.service';
 
 @Module({
   imports: [],
@@ -9,7 +10,8 @@ import { CultureContentLikeCoreRepository } from 'libs/core/culture-content/cult
     CultureContentCoreService,
     CultureContentCoreRepository,
     CultureContentLikeCoreRepository,
+    CultureContentLikeCoreService,
   ],
-  exports: [CultureContentCoreService],
+  exports: [CultureContentCoreService, CultureContentLikeCoreService],
 })
 export class CultureContentCoreModule {}
