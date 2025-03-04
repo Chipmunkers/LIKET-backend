6a970665600383345eaa8b2e633404b91e54be8a
jochongs <joch2712@naver.com>
2025-03-04 16:08:47 +0900

    feat(core): Implement likeReviewByIdx core method

--- libs/core/src/review/review-like-core.service.ts
+++ libs/core/src/review/review-like-core.service.ts
@@ -1,4 +1,6 @@
+import { Transactional } from '@nestjs-cls/transactional';
 import { Injectable } from '@nestjs/common';
+import { AlreadyLikedReviewException } from 'libs/core/review/exception/AlreadyLikedReviewException';
 import { ReviewLikeCoreRepository } from 'libs/core/review/review-like-core.repository';
 
 @Injectable()
@@ -6,4 +8,33 @@ export class ReviewLikeCoreService {
   constructor(
     private readonly reviewLikeCoreRepository: ReviewLikeCoreRepository,
   ) {}
+
+  /**
+   * 리뷰 좋아요 추가하기
+   *
+   * @author jochongs
+   *
+   * @param userIdx 좋아요를 누른 사용자 인덱스
+   * @param reviewIdx 리뷰 인덱스
+   */
+  @Transactional()
+  public async likeReviewByIdx(
+    userIdx: number,
+    reviewIdx: number,
+  ): Promise<void> {
+    const likeReview = await this.reviewLikeCoreRepository.selectReviewLike(
+      userIdx,
+      reviewIdx,
+    );
+
+    if (likeReview) {
+      throw new AlreadyLikedReviewException(userIdx, reviewIdx);
+    }
+
+    await this.reviewLikeCoreRepository.insertReviewLike(userIdx, reviewIdx);
+    await this.reviewLikeCoreRepository.increaseReviewLikeCountByIdx(
+      reviewIdx,
+      1,
+    );
+  }
 }
