00ce6982d084068cffa9253b11ee5c58c038e9c3
jochongs <joch2712@naver.com>
2025-03-06 16:26:51 +0900

    feat(core): Add ReviewReportCoreRepository, ReviewReportCoreService

--- libs/core/src/review/review-core.module.ts
+++ libs/core/src/review/review-core.module.ts
@@ -3,6 +3,8 @@ import { ReviewCoreRepository } from 'libs/core/review/review-core.repository';
 import { ReviewCoreService } from 'libs/core/review/review-core.service';
 import { ReviewLikeCoreRepository } from 'libs/core/review/review-like-core.repository';
 import { ReviewLikeCoreService } from 'libs/core/review/review-like-core.service';
+import { ReviewReportCoreRepository } from 'libs/core/review/review-report-core.repository';
+import { ReviewReportCoreService } from 'libs/core/review/review-report-core.service';
 
 @Module({
   imports: [],
@@ -11,7 +13,9 @@ import { ReviewLikeCoreService } from 'libs/core/review/review-like-core.service
     ReviewCoreRepository,
     ReviewLikeCoreService,
     ReviewLikeCoreRepository,
+    ReviewReportCoreRepository,
+    ReviewReportCoreService,
   ],
-  exports: [ReviewCoreService, ReviewLikeCoreService],
+  exports: [ReviewCoreService, ReviewLikeCoreService, ReviewReportCoreService],
 })
 export class ReviewCoreModule {}
