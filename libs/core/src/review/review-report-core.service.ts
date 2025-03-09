42477c8c7a0a575980c4c9d20822d2dd3e684b0b
jochongs <joch2712@naver.com>
2025-03-09 15:42:00 +0900

    chore(core): Add ReviewReportCoreRepository property to service

--- libs/core/src/review/review-report-core.service.ts
+++ libs/core/src/review/review-report-core.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ReviewReportCoreRepository } from 'libs/core/review/review-report-core.repository';
 
 @Injectable()
 export class ReviewReportCoreService {
-  constructor() {}
+  constructor(
+    private readonly reviewReportCoreRepository: ReviewReportCoreRepository,
+  ) {}
 }
