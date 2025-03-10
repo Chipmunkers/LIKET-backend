b8b1b5b78669d6c061c8b4752f089a1a2adad0d6
jochongs <joch2712@naver.com>
2025-03-10 17:55:22 +0900

    feat(core): Add FindReportedReviewAllInput input class

---
+++ libs/core/src/review/input/find-reported-review-all.input.ts
@@ -0,0 +1,24 @@
+/**
+ * @author jochongs
+ */
+export class FindReportedReviewAllInput {
+  /** 페이지 번호 (1부터 시작) */
+  public readonly page: number;
+
+  /** 한 번에 가져올 데이터 개수 */
+  public readonly row: number;
+
+  /** 검색할 요소 */
+  public readonly searchBy?: ('review' | 'author')[];
+
+  /** 검색 키워드 */
+  public readonly search?: string;
+
+  /**
+   * 리뷰 신고 상태 필터링
+   *
+   * true: 삭제 처리가 된 리뷰만 가져오기
+   * false: 삭제 처리가 안 된 리뷰만 가져오기
+   */
+  public readonly state?: boolean;
+}
