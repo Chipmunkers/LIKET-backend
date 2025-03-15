0959e4788821e94320fe8a3c256b9fd4b3435650
jochongs <joch2712@naver.com>
2025-03-15 19:58:07 +0900

    feat(core): Implement insertInterestGenre, deleteInterestGenre method

--- libs/core/src/user/user-core.module.ts
+++ libs/core/src/user/user-core.module.ts
@@ -3,10 +3,17 @@ import { UserCoreRepository } from 'libs/core/user/user-core.repository';
 import { UserCoreService } from 'libs/core/user/user-core.service';
 import { HashModule } from 'libs/modules/hash/hash.module';
 import { WithdrawalReasonCoreModule } from 'libs/core/withdrawal-reason/withdrawal-reason-core.module';
+import { UserInterestCoreService } from 'libs/core/user/user-interest.service';
+import { UserInterestCoreRepository } from 'libs/core/user/user-interest.repository';
 
 @Module({
   imports: [HashModule, WithdrawalReasonCoreModule],
-  providers: [UserCoreService, UserCoreRepository],
+  providers: [
+    UserCoreService,
+    UserCoreRepository,
+    UserInterestCoreService,
+    UserInterestCoreRepository,
+  ],
   exports: [UserCoreService],
 })
 export class UserCoreModule {}
---
+++ libs/core/src/user/user-interest.repository.ts
@@ -0,0 +1,46 @@
+import { PrismaProvider } from 'libs/modules';
+import { Injectable } from '@nestjs/common';
+import { TransactionHost } from '@nestjs-cls/transactional';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { Genre } from 'libs/core/tag-root/genre/constant/genre';
+
+@Injectable()
+export class UserInterestCoreRepository {
+  constructor(
+    private readonly txHost: TransactionHost<
+      TransactionalAdapterPrisma<PrismaProvider>
+    >,
+  ) {}
+
+  /**
+   * INSERT interest_genre_tb
+   *
+   * @author jochongs
+   *
+   * @param idx 사용자 식별자
+   */
+  public async insertInterestGenre(
+    idx: number,
+    genreList: Genre[],
+  ): Promise<void> {
+    await this.txHost.tx.interestGenre.createMany({
+      data: genreList.map((genreIdx) => ({
+        genreIdx,
+        userIdx: idx,
+      })),
+    });
+  }
+
+  /**
+   * DELETE interest_genre_tb WHERE idx = $1
+   *
+   * @author jochongs
+   *
+   * @param idx 사용자 식별자
+   */
+  public async deleteInterestGenre(idx: number): Promise<void> {
+    await this.txHost.tx.interestGenre.deleteMany({
+      where: { userIdx: idx },
+    });
+  }
+}
