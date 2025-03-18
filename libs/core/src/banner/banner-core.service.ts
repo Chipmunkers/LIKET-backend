39725805d1d9b18134cfdcbc7eb86d6ffeaf69c3
jochongs <joch2712@naver.com>
2025-03-18 13:36:37 +0900

    feat(core): Implement findBannerAll method

--- libs/core/src/banner/banner-core.service.ts
+++ libs/core/src/banner/banner-core.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { BannerCoreRepository } from 'libs/core/banner/banner-core.repository';
+import { FindBannerAllInput } from 'libs/core/banner/input/find-banner-all.input';
+import { BannerModel } from 'libs/core/banner/model/banner.model';
 
 @Injectable()
-export class BannerCoreService {}
+export class BannerCoreService {
+  constructor(private readonly bannerCoreRepository: BannerCoreRepository) {}
+
+  /**
+   * 배너 목록보기
+   *
+   * @author jochongs
+   */
+  public async findBannerAll(
+    input: FindBannerAllInput,
+  ): Promise<BannerModel[]> {
+    return (await this.bannerCoreRepository.selectBannerAll(input)).map(
+      BannerModel.fromPrisma,
+    );
+  }
+}
