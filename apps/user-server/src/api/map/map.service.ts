b7dc3fce4796b057399f2d48147598cd9db9333f
jochongs <joch2712@naver.com>
2025-03-18 09:09:18 +0900

    chore(user-server): Update getClusteredContentsAllForMap method deprecated

--- apps/user-server/src/api/map/map.service.ts
+++ apps/user-server/src/api/map/map.service.ts
@@ -10,6 +10,11 @@ export class MapService {
   constructor(private readonly mapRepository: MapRepository) {}
 
   /**
+   * 클러스터링 레벨에 따라 활성화된 컨텐츠 개수를 가져오는 메서드
+   * !주의: 이 API는 deprecated 되었습니다. 대신 getContentAll 메서드만 사용하십시오.
+   *
+   * @deprecated
+   *
    * @author jochongs
    */
   public async getClusteredContentsAllForMap(pagerble: MapPagerbleDto) {
