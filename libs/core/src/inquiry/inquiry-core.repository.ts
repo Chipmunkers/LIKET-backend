7f9538f62573538b846ba2836e16018504b74288
jochongs <joch2712@naver.com>
2025-03-31 20:31:36 +0900

    feat(core): Implement selectInquiryByIdx method

--- libs/core/src/inquiry/inquiry-core.repository.ts
+++ libs/core/src/inquiry/inquiry-core.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { PrismaProvider } from 'libs/modules';
+import { InquirySelectField } from 'libs/core/inquiry/model/prisma/inquiry-select-field';
 
 @Injectable()
 export class InquiryCoreRepository {
@@ -10,4 +11,59 @@ export class InquiryCoreRepository {
       TransactionalAdapterPrisma<PrismaProvider>
     >,
   ) {}
+
+  /**
+   * SELECT inquiry_tb WHERE idx = $1
+   *
+   * @author jochongs
+   *
+   * @param idx 문의 식별자
+   */
+  public async selectInquiryByIdx(
+    idx: number,
+  ): Promise<InquirySelectField | null> {
+    return await this.txHost.tx.inquiry.findUnique({
+      select: {
+        idx: true,
+        title: true,
+        contents: true,
+        createdAt: true,
+        InquiryType: {
+          select: {
+            idx: true,
+            name: true,
+          },
+        },
+        InquiryImg: {
+          select: {
+            idx: true,
+            imgPath: true,
+            createdAt: true,
+          },
+          orderBy: { idx: 'asc' },
+          where: { deletedAt: null },
+        },
+        User: {
+          select: {
+            idx: true,
+            nickname: true,
+            email: true,
+            profileImgPath: true,
+          },
+        },
+        Answer: {
+          select: {
+            idx: true,
+            contents: true,
+            createdAt: true,
+          },
+        },
+      },
+      where: {
+        idx,
+        deletedAt: null,
+        User: { deletedAt: null },
+      },
+    });
+  }
 }
