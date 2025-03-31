abe0bf4fcb7448727ea5db5e75dd3ae4a87e65bd
jochongs <joch2712@naver.com>
2025-03-31 21:12:53 +0900

    feat(core): Implement findInquiryAll method

--- libs/core/src/inquiry/inquiry-core.service.ts
+++ libs/core/src/inquiry/inquiry-core.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { FindInquiryAllInput } from 'libs/core/inquiry/input/find-inquiry-all.input';
+import { InquiryCoreRepository } from 'libs/core/inquiry/inquiry-core.repository';
+import { InquiryModel } from 'libs/core/inquiry/model/inquiry.model';
+import { SummaryInquiryModel } from 'libs/core/inquiry/model/summary-inquiry.model';
 
 @Injectable()
 export class InquiryCoreService {
-  constructor() {}
+  constructor(private readonly inquiryCoreRepository: InquiryCoreRepository) {}
+
+  /**
+   * 문의 목록 불러오기
+   *
+   * @author jochongs
+   */
+  public async findInquiryAll(
+    input: FindInquiryAllInput,
+  ): Promise<SummaryInquiryModel[]> {
+    return (await this.inquiryCoreRepository.selectInquiryAll(input)).map(
+      SummaryInquiryModel.fromPrisma,
+    );
+  }
 }
