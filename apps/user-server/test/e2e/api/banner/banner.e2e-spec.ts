160a16c3c2440bb1699c54b7ac1bbf5c13c0dbd2
jochongs <joch2712@naver.com>
2025-04-01 13:07:07 +0900

    test(user-server): Add test case about GET /banner/all API

--- apps/user-server/test/e2e/api/banner/banner.e2e-spec.ts
+++ apps/user-server/test/e2e/api/banner/banner.e2e-spec.ts
@@ -1,9 +1,12 @@
+import { BannerEntity } from 'apps/user-server/src/api/banner/entity/banner.entity';
 import { AppModule } from 'apps/user-server/src/app.module';
 import { TestHelper } from 'apps/user-server/test/e2e/setup/test.helper';
+import { BannerSeedHelper } from 'libs/testing/seed/banner/banner-seed.helper';
 import * as request from 'supertest';
 
 describe('Banner (e2e)', () => {
   const test = TestHelper.create(AppModule);
+  const bannerSeedHelper = test.seedHelper(BannerSeedHelper);
 
   beforeEach(async () => {
     await test.init();
@@ -21,6 +24,36 @@ describe('Banner (e2e)', () => {
 
       expect(response.body?.bannerList).toBeDefined();
       expect(Array.isArray(response.body?.bannerList)).toBe(true);
+    });
+
+    it('Success - banner order test', async () => {
+      const [banner1, banner2, notActivatedBanner] =
+        await bannerSeedHelper.seedAll([
+          {
+            order: 2,
+          },
+          {
+            order: 1,
+          },
+          {
+            order: null,
+          },
+          {
+            order: null,
+            deletedAt: new Date(),
+          },
+        ]);
+
+      const response = await request(test.getServer())
+        .get('/banner/all')
+        .expect(200);
+
+      const bannerList: BannerEntity[] = response.body.bannerList;
+
+      expect(bannerList.map(({ idx }) => idx)).toStrictEqual([
+        banner2.idx,
+        banner1.idx,
+      ]);
     });
 
     it('Success with token', async () => {
