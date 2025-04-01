2720328f8cb8bc2f67828a1bc11555ec8fca7b9e
jochongs <joch2712@naver.com>
2025-04-01 11:53:15 +0900

    refactor(user-server): Delete PrismaProvider depedency from InquiryModule

--- apps/user-server/src/api/inquiry/inquiry.module.ts
+++ apps/user-server/src/api/inquiry/inquiry.module.ts
@@ -3,11 +3,10 @@ import { InquiryService } from './inquiry.service';
 import { InquiryController } from './inquiry.controller';
 import { InquiryTypeController } from './inquiry-type.controller';
 import { InquiryTypeService } from './inquiry-type.service';
-import { PrismaModule } from 'libs/modules';
 import { InquiryCoreModule } from 'libs/core/inquiry/inquiry-core.module';
 
 @Module({
-  imports: [PrismaModule, InquiryCoreModule],
+  imports: [InquiryCoreModule],
   providers: [InquiryService, InquiryTypeService],
   controllers: [InquiryController, InquiryTypeController],
 })
