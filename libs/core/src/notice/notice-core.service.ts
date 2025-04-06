f49189bb55e291716a04ab55eff71c7a8869a5d7
jochongs <joch2712@naver.com>
2025-04-06 18:06:56 +0900

    feat(core): Implement findNoticeByIdx service method

--- libs/core/src/notice/notice-core.service.ts
+++ libs/core/src/notice/notice-core.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { FindNoticeAllInput } from 'libs/core/notice/input/find-notice-all.input';
+import { NoticeModel } from 'libs/core/notice/model/notice.model';
 import { SummaryNoticeModel } from 'libs/core/notice/model/summary-notice.model';
 import { NoticeCoreRepository } from 'libs/core/notice/notice-core.repository';
 
@@ -18,5 +19,18 @@ export class NoticeCoreService {
     return (await this.noticeCoreRepository.selectNoticeAll(input)).map(
       SummaryNoticeModel.fromPrisma,
     );
+  }
+
+  /**
+   * 공지사항 자세히보기
+   *
+   * @author jochongs
+   *
+   * @param idx 공지사항 식별자
+   */
+  public async findNoticeByIdx(idx: number): Promise<NoticeModel | null> {
+    const notice = await this.noticeCoreRepository.selectNoticeByIdx(idx);
+
+    return notice && NoticeModel.fromPrisma(notice);
   }
 }
