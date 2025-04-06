1c25f297a85c688b9e833f974a0918c381adccd3
jochongs <joch2712@naver.com>
2025-04-06 17:06:56 +0900

    feat(core): Implement selectNoticeByIdx method

--- libs/core/src/notice/notice-core.repository.ts
+++ libs/core/src/notice/notice-core.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { PrismaProvider } from 'libs/modules';
+import { NoticeSelectField } from 'libs/core/notice/model/prisma/notice-select-field';
 
 @Injectable()
 export class NoticeCoreRepository {
@@ -10,4 +11,30 @@ export class NoticeCoreRepository {
       TransactionalAdapterPrisma<PrismaProvider>
     >,
   ) {}
+
+  /**
+   * SELECT notice_tb WHERE idx = $1
+   *
+   * @author jochongs
+   *
+   * @param idx 공지사항 식별자
+   */
+  public async selectNoticeByIdx(
+    idx: number,
+  ): Promise<NoticeSelectField | null> {
+    return await this.txHost.tx.notice.findUnique({
+      select: {
+        idx: true,
+        title: true,
+        contents: true,
+        pinnedAt: true,
+        activatedAt: true,
+        createdAt: true,
+      },
+      where: {
+        idx,
+        deletedAt: null,
+      },
+    });
+  }
 }
