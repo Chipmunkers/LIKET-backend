80678c93d94b8ba4bda2f37d3eaab70f7a7ece32
jochongs <joch2712@naver.com>
2025-04-06 18:05:24 +0900

    feat(core): Implement findNoticeAll service method

--- libs/core/src/notice/notice-core.service.ts
+++ libs/core/src/notice/notice-core.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { FindNoticeAllInput } from 'libs/core/notice/input/find-notice-all.input';
+import { SummaryNoticeModel } from 'libs/core/notice/model/summary-notice.model';
 import { NoticeCoreRepository } from 'libs/core/notice/notice-core.repository';
 
 @Injectable()
 export class NoticeCoreService {
   constructor(private readonly noticeCoreRepository: NoticeCoreRepository) {}
+
+  /**
+   * 공지사항 목록보기
+   *
+   * @author jochongs
+   */
+  public async findNoticeAll(
+    input: FindNoticeAllInput,
+  ): Promise<SummaryNoticeModel[]> {
+    return (await this.noticeCoreRepository.selectNoticeAll(input)).map(
+      SummaryNoticeModel.fromPrisma,
+    );
+  }
 }
