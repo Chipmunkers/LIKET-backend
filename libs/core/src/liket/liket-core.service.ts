0cf762f00eaeb65118eea4baa41fcc1419ea8261
jochongs <joch2712@naver.com>
2025-04-16 14:54:34 +0900

    feat(core): Implement findLiketAll method

--- libs/core/src/liket/liket-core.service.ts
+++ libs/core/src/liket/liket-core.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { FindLiketAllInput } from 'libs/core/liket/input/find-liket-all.input';
+import { LiketCoreRepository } from 'libs/core/liket/liket-core.repository';
+import { SummaryLiketModel } from 'libs/core/liket/model/summary-liket.model';
 
 @Injectable()
 export class LiketCoreService {
-  constructor() {}
+  constructor(private readonly liketCoreRepository: LiketCoreRepository) {}
+
+  /**
+   * 라이켓 목록 가져오기
+   *
+   * @author jochongs
+   */
+  public async findLiketAll(
+    input: FindLiketAllInput,
+  ): Promise<SummaryLiketModel[]> {
+    return (await this.liketCoreRepository.selectLiketAll(input)).map(
+      SummaryLiketModel.fromPrisma,
+    );
+  }
 }
