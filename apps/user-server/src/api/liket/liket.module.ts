5133b4aade27c313df7f58f8faeef64a52069672
jochongs <joch2712@naver.com>
2025-04-18 19:13:20 +0900

    refactor(user-server): Delete liket.repository file

--- apps/user-server/src/api/liket/liket.module.ts
+++ apps/user-server/src/api/liket/liket.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { LiketService } from './liket.service';
 import { LiketController } from './liket.controller';
 import { LiketAuthService } from './liket-auth.service';
-import { LiketRepository } from './liket.repository';
 import { PrismaModule } from 'libs/modules';
 import { LiketCoreModule } from 'libs/core/liket/liket-core.module';
 import { ReviewCoreModule } from 'libs/core/review/review-core.module';
@@ -10,6 +9,6 @@ import { ReviewCoreModule } from 'libs/core/review/review-core.module';
 @Module({
   imports: [PrismaModule, LiketCoreModule, ReviewCoreModule],
   controllers: [LiketController],
-  providers: [LiketService, LiketAuthService, LiketRepository],
+  providers: [LiketService, LiketAuthService],
 })
 export class LiketModule {}
