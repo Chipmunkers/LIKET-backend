62daa361767ee9001a3b4e5f4d0b0b50e4142c09
jochongs <joch2712@naver.com>
2025-04-26 17:53:46 +0900

    feat(core): Implement findTosAll method

--- libs/core/src/tos/tos-core.service.ts
+++ libs/core/src/tos/tos-core.service.ts
@@ -1,11 +1,24 @@
 import { Transactional } from '@nestjs-cls/transactional';
 import { Injectable } from '@nestjs/common';
+import { FindTosAllInput } from 'libs/core/tos/input/find-tos-all.input';
+import { SummaryTosModel } from 'libs/core/tos/model/summary-tos.model';
 import { TosModel } from 'libs/core/tos/model/tos.model';
 import { TosCoreRepository } from 'libs/core/tos/tos-core.repository';
 
 @Injectable()
 export class TosCoreService {
   constructor(private readonly tosCoreRepository: TosCoreRepository) {}
+
+  /**
+   * 약관 목록보기
+   *
+   * @author jochongs
+   */
+  public async findTosAll(input: FindTosAllInput): Promise<SummaryTosModel[]> {
+    return (await this.tosCoreRepository.selectTosAll(input)).map(
+      SummaryTosModel.fromPrisma,
+    );
+  }
 
   /**
    * 약관 가져오기
