b5956c1b73d96a03bf0aa23169d214e02fea96f4
jochongs <joch2712@naver.com>
2025-04-26 17:52:02 +0900

    feat(core): Implement findTosByIdx method

--- libs/core/src/tos/tos-core.service.ts
+++ libs/core/src/tos/tos-core.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { TosModel } from 'libs/core/tos/model/tos.model';
 import { TosCoreRepository } from 'libs/core/tos/tos-core.repository';
 
 @Injectable()
 export class TosCoreService {
   constructor(private readonly tosCoreRepository: TosCoreRepository) {}
+
+  /**
+   * 약관 가져오기
+   *
+   * @author jochongs
+   *
+   * @param idx 약관 식별자
+   */
+  public async findTosByIdx(idx: number): Promise<TosModel | null> {
+    const tos = await this.tosCoreRepository.selectTosByIdx(idx);
+
+    return tos && TosModel.fromPrisma(tos);
+  }
 }
